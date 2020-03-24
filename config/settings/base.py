d1c31a574787afa3fba800ae51272b526a2a7323
anderson89marques <andersonoanjo18@gmail.com>
2020-03-24 16:06:33 -0300

    Autenticaçao usando a API sme-autentica-coresso

--- config/settings/base.py
+++ config/settings/base.py
@@ -307,6 +307,7 @@ STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
 # django-rest-framework - https://www.django-rest-framework.org/api-guide/settings/
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
