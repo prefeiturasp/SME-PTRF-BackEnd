7c6c8e59e9e18ea9b0a4cfab5b62734592dd1281
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-03-25 18:06:03 -0300

    feat(despesas): Implementa get rateios-despesas
    Lista de rateios de despesa.

--- sme_ptrf_apps/despesas/api/serializers/rateio_despesa_serializer.py
+++ sme_ptrf_apps/despesas/api/serializers/rateio_despesa_serializer.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
-from .especificacao_material_servico_serializer import EspecificacaoMaterialServicoSerializer
+from .especificacao_material_servico_serializer import EspecificacaoMaterialServicoSerializer, \
+    EspecificacaoMaterialServicoLookUpSerializer
 from .tipo_custeio_serializer import TipoCusteioSerializer
 from ...models import RateioDespesa, Despesa
-from ....core.api.serializers.acao_associacao_serializer import AcaoAssociacaoSerializer
+from ....core.api.serializers.acao_associacao_serializer import AcaoAssociacaoSerializer, AcaoAssociacaoLookUpSerializer
 from ....core.api.serializers.associacao_serializer import AssociacaoSerializer
 from ....core.api.serializers.conta_associacao_serializer import ContaAssociacaoSerializer
 from ....core.models import Associacao, ContaAssociacao, AcaoAssociacao
@@ -45,7 +46,45 @@ class RateioDespesaCreateSerializer(serializers.ModelSerializer):
         queryset=AcaoAssociacao.objects.all()
     )
 
-
     class Meta:
         model = RateioDespesa
         exclude = ('id', 'despesa')
+
+
+class RateioDespesaListaSerializer(serializers.ModelSerializer):
+    despesa = serializers.SlugRelatedField(
+        slug_field='uuid',
+        required=False,
+        queryset=Despesa.objects.all()
+    )
+    acao_associacao = AcaoAssociacaoLookUpSerializer()
+    especificacao_material_servico = EspecificacaoMaterialServicoLookUpSerializer()
+    numero_documento = serializers.SerializerMethodField('get_numero_documento')
+    status_despesa = serializers.SerializerMethodField('get_status_despesa')
+    data_documento = serializers.SerializerMethodField('get_data_documento')
+    valor_total = serializers.SerializerMethodField('get_valor_total')
+
+    def get_numero_documento(self, rateio):
+        return rateio.despesa.numero_documento
+
+    def get_status_despesa(self, rateio):
+        return rateio.despesa.status
+
+    def get_data_documento(self, rateio):
+        return rateio.despesa.data_documento
+
+    def get_valor_total(self, rateio):
+        return rateio.despesa.valor_total
+
+    class Meta:
+        model = RateioDespesa
+        fields = (
+            'uuid',
+            'despesa',
+            'numero_documento',
+            'status_despesa',
+            'especificacao_material_servico',
+            'data_documento', 'aplicacao_recurso',
+            'acao_associacao',
+            'valor_total'
+        )
--- sme_ptrf_apps/despesas/tests/tests_rateio_despesa/test_rateio_despesa_serializer.py
+++ sme_ptrf_apps/despesas/tests/tests_rateio_despesa/test_rateio_despesa_serializer.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ...api.serializers.rateio_despesa_serializer import (RateioDespesaSerializer)
+from ...api.serializers.rateio_despesa_serializer import (RateioDespesaSerializer, RateioDespesaListaSerializer)
 
 pytestmark = pytest.mark.django_db
 
@@ -22,3 +22,18 @@ def test_serializer(rateio_despesa_capital):
     assert serializer.data['quantidade_itens_capital']
     assert serializer.data['valor_item_capital']
     assert serializer.data['numero_processo_incorporacao_capital']
+
+
+def test_serializer_lista(rateio_despesa_capital):
+    serializer = RateioDespesaListaSerializer(rateio_despesa_capital)
+
+    assert serializer.data is not None
+    assert serializer.data['uuid']
+    assert serializer.data['despesa']
+    assert serializer.data['numero_documento']
+    assert serializer.data['status_despesa']
+    assert serializer.data['especificacao_material_servico']
+    assert serializer.data['data_documento']
+    assert serializer.data['aplicacao_recurso']
+    assert serializer.data['acao_associacao']
+    assert serializer.data['valor_total']
