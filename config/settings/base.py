e71f32b1dce714b4825412859a220642215bf80c
anderson89marques <andersonoanjo18@gmail.com>
2020-04-01 21:40:46 -0300

    Retira SessionAuthentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -313,7 +313,7 @@ STATICFILES_FINDERS += ["compressor.finders.CompressorFinder"]
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
+        #"rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
     # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
