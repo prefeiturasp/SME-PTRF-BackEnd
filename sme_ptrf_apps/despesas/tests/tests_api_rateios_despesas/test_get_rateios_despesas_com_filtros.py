69d6d89720ff486703a5b25ead00dc59d32b4fe6
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-04-08 16:00:15 -0300

    feat(13076): Filtro por status da despesa em rateios de despesa
    Implementa o filtro por status da despesa na consulta de rateios de
    despesa.

--- sme_ptrf_apps/despesas/tests/tests_api_rateios_despesas/test_get_rateios_despesas_com_filtros.py
+++ sme_ptrf_apps/despesas/tests/tests_api_rateios_despesas/test_get_rateios_despesas_com_filtros.py
@@ -1,6 +1,7 @@
 import json
 
 import pytest
+from model_bakery import baker
 from rest_framework import status
 
 pytestmark = pytest.mark.django_db
@@ -54,3 +55,93 @@ def test_api_get_despesas_filtro_por_acao_associacao(client, associacao, despesa
 
     assert response.status_code == status.HTTP_200_OK
     assert len(result) == 1
+
+
+@pytest.fixture
+def despesa_incompleta(associacao, tipo_documento, tipo_transacao):
+    return baker.make(
+        'Despesa',
+        associacao=associacao,
+        numero_documento='123456',
+        data_documento=None,
+        tipo_documento=tipo_documento,
+        cpf_cnpj_fornecedor='11.478.276/0001-04',
+        nome_fornecedor='Fornecedor SA',
+        tipo_transacao=tipo_transacao,
+        data_transacao=None,
+        valor_total=100.00,
+        valor_recursos_proprios=10.00,
+    )
+
+
+@pytest.fixture
+def rateio_despesa_capital_completo(associacao, despesa_incompleta, conta_associacao, acao,
+                                    tipo_aplicacao_recurso_capital,
+                                    especificacao_material_servico, acao_associacao):
+    return baker.make(
+        'RateioDespesa',
+        despesa=despesa_incompleta,
+        associacao=associacao,
+        conta_associacao=conta_associacao,
+        acao_associacao=acao_associacao,
+        aplicacao_recurso=tipo_aplicacao_recurso_capital,
+        tipo_custeio=None,
+        especificacao_material_servico=especificacao_material_servico,
+        valor_rateio=10.00,  # Falta o valor
+        quantidade_itens_capital=1,
+        valor_item_capital=10.00,
+        numero_processo_incorporacao_capital='teste123456'
+    )
+
+
+@pytest.fixture
+def rateio_despesa_capital_incompleto(associacao, despesa_incompleta, conta_associacao, acao,
+                                      tipo_aplicacao_recurso_capital,
+                                      especificacao_material_servico, acao_associacao):
+    return baker.make(
+        'RateioDespesa',
+        despesa=despesa_incompleta,
+        associacao=associacao,
+        conta_associacao=conta_associacao,
+        acao_associacao=acao_associacao,
+        aplicacao_recurso=tipo_aplicacao_recurso_capital,
+        tipo_custeio=None,
+        especificacao_material_servico=especificacao_material_servico,
+        valor_rateio=0.00,
+        quantidade_itens_capital=0,
+        valor_item_capital=0,
+        numero_processo_incorporacao_capital=''
+    )
+
+
+@pytest.fixture
+def rateio_despesa_custeio_completo(associacao, despesa, conta_associacao, acao, tipo_aplicacao_recurso_custeio,
+                                    tipo_custeio_servico, especificacao_material_servico, acao_associacao):
+    return baker.make(
+        'RateioDespesa',
+        despesa=despesa,
+        associacao=associacao,
+        conta_associacao=conta_associacao,
+        acao_associacao=acao_associacao,
+        aplicacao_recurso=tipo_aplicacao_recurso_custeio,
+        tipo_custeio=tipo_custeio_servico,
+        especificacao_material_servico=especificacao_material_servico,
+        valor_rateio=100.00,
+
+    )
+
+
+def test_api_get_despesas_filtro_por_status(client, associacao, despesa_incompleta, rateio_despesa_capital_completo,
+                                            rateio_despesa_capital_incompleto, despesa,
+                                            rateio_despesa_custeio_completo):
+    response = client.get('/api/rateios-despesas/?despesa__status=COMPLETO', content_type='application/json')
+    result = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(result) == 1
+
+    response = client.get('/api/rateios-despesas/?despesa__status=INCOMPLETO', content_type='application/json')
+    result = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(result) == 2
