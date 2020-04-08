902ef97bfd33926e613a33f962110c43e34809ff
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-04-08 14:31:47 -0300

    refactor(13076): Refatoração de testes
    Altera pasta e nome de testes.

---
+++ sme_ptrf_apps/despesas/tests/tests_api_rateios_despesas/test_get_rateios_despesas_search_por_especificacao.py
@@ -0,0 +1,20 @@
+import json
+
+import pytest
+from rest_framework import status
+
+pytestmark = pytest.mark.django_db
+
+
+def test_api_get_search_despesas_por_especificacao(client, associacao, despesa, conta_associacao, acao,
+                                                   tipo_aplicacao_recurso_custeio,
+                                                   tipo_custeio_servico,
+                                                   especificacao_instalacao_eletrica, acao_associacao,
+                                                   especificacao_material_eletrico,
+                                                   rateio_despesa_material_eletrico,
+                                                   rateio_despesa_instalacao_eletrica):
+    response = client.get('/api/rateios-despesas/?search=elétrico', content_type='application/json')
+    result = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(result) == 1
