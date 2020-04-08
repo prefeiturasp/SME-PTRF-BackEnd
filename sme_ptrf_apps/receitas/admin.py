8b366941e14fd9c027ad9aa1f1d2bc1ff154db9e
anderson89marques <andersonoanjo18@gmail.com>
2020-04-08 17:33:20 -0300

    Filtro em receitas no admin.

--- sme_ptrf_apps/receitas/admin.py
+++ sme_ptrf_apps/receitas/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 
 from sme_ptrf_apps.receitas.models import Receita, TipoReceita
 
+from rangefilter.filter import DateRangeFilter
+
 admin.site.register(TipoReceita)
+
+
+def customTitledFilter(title):
+   class Wrapper(admin.FieldListFilter):
+       def __new__(cls, *args, **kwargs):
+           instance = admin.FieldListFilter.create(*args, **kwargs)
+           instance.title = title
+           return instance
+   return Wrapper
 
 
 @admin.register(Receita)
@@ -10,5 +21,10 @@ class ReceitaAdmin(admin.ModelAdmin):
     list_display = ('data', 'valor', 'descricao', 'associacao',)
     ordering = ('-data',)
     search_fields = ('descricao',)
-    list_filter = ('data',)
+    list_filter = (
+        ('data', DateRangeFilter),
+        ('associacao__nome', customTitledFilter('Associação')), 
+        ('acao_associacao__acao__nome', customTitledFilter('Ação')), 
+        ('conta_associacao__tipo_conta__nome', customTitledFilter('Tipo Conta')),
+        ('tipo_receita', customTitledFilter('Tipo Receita')))
     readonly_fields = ('uuid', 'id')
