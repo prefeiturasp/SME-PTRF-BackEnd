8b67f1cd8c52ad91423b1f5f67854113c8e00945
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-05-14 10:44:32 -0300

    feat(15110): Serviço concluir prestação de contas
    Implementa o serviço responsável por concluir uma prestação de contas.

--- sme_ptrf_apps/core/services/prestacao_contas_services.py
+++ sme_ptrf_apps/core/services/prestacao_contas_services.py
@@ -1,14 +1,37 @@
-from ..models import PrestacaoConta, ContaAssociacao, Periodo
+from ..models import PrestacaoConta, ContaAssociacao, Periodo, AcaoAssociacao, FechamentoPeriodo
+from ...despesas.models import RateioDespesa
+from ...receitas.models import Receita
 
 
 def iniciar_prestacao_de_contas(conta_associacao_uuid, periodo_uuid):
     conta_associacao = ContaAssociacao.objects.get(uuid=conta_associacao_uuid)
-    periodo = Periodo.objects.get(uuid=periodo_uuid)
+    periodo = Periodo.by_uuid(periodo_uuid)
 
     return PrestacaoConta.iniciar(conta_associacao=conta_associacao, periodo=periodo)
 
 def concluir_prestacao_de_contas(prestacao_contas_uuid, observacoes):
-    return PrestacaoConta.concluir(uuid=prestacao_contas_uuid, observacoes=observacoes)
+
+    prestacao = PrestacaoConta.concluir(uuid=prestacao_contas_uuid, observacoes=observacoes)
+
+    associacao = prestacao.associacao
+    periodo = prestacao.periodo
+    acoes = associacao.acoes.filter(status=AcaoAssociacao.STATUS_ATIVA)
+
+    for acao in acoes:
+        totais_receitas = Receita.totais_por_acao_associacao_no_periodo(acao_associacao=acao, periodo=periodo)
+        totais_despesas = RateioDespesa.totais_por_acao_associacao_no_periodo(acao_associacao=acao, periodo=periodo)
+        FechamentoPeriodo.criar(
+            prestacao_conta=prestacao,
+            acao_associacao=acao,
+            total_receitas_capital=totais_receitas['total_receitas_capital'],
+            total_repasses_capital=totais_receitas['total_repasses_capital'],
+            total_receitas_custeio=totais_receitas['total_receitas_custeio'],
+            total_repasses_custeio=totais_receitas['total_repasses_custeio'],
+            total_despesas_capital=totais_despesas['total_despesas_capital'],
+            total_despesas_custeio=totais_despesas['total_despesas_custeio'],
+        )
+
+    return prestacao
 
 def salvar_prestacao_de_contas(prestacao_contas_uuid):
     ...
--- sme_ptrf_apps/core/tests/tests_services/test_concluir_prestacao_de_contas_service.py
+++ sme_ptrf_apps/core/tests/tests_services/test_concluir_prestacao_de_contas_service.py
@@ -16,12 +16,90 @@ def test_prestacao_de_contas_deve_ser_atualizada(prestacao_conta_iniciada):
     assert prestacao.conciliado, "Deveria ter sido marcada como conciliado."
     assert prestacao.conciliado_em is not None, "Deveria ter gravado a data e hora da última conciliação."
 
-# def test_fechamentos_devem_ser_criados(conta_associacao,
-#                                        periodo_2020_1,
-#                                        receita_conferida,
-#                                        receita_nao_conferida,
-#                                        rateio_despesa_conferido,
-#                                        rateio_despesa_nao_conferido):
-#     prestacao = iniciar_prestacao_de_contas(conta_associacao_uuid=conta_associacao.uuid, periodo_uuid=periodo_2020_1.uuid)
-#
-#     assert prestacao.fechamentos_da_prestacao.exists()
+
+def test_fechamentos_devem_ser_criados_por_acao(prestacao_conta_iniciada,
+                                                periodo_2020_1,
+                                                receita_2020_1_role_repasse_custeio_conferida,
+                                                receita_2020_1_ptrf_repasse_capital_conferida,
+                                                receita_2020_1_role_repasse_capital_nao_conferida,
+                                                receita_2019_2_role_repasse_capital_conferida,
+                                                receita_2020_1_role_repasse_capital_conferida,
+                                                receita_2020_1_role_rendimento_custeio_conferida,
+                                                despesa_2020_1,
+                                                rateio_despesa_2020_role_custeio_conferido,
+                                                rateio_despesa_2020_role_custeio_nao_conferido,
+                                                rateio_despesa_2020_role_capital_conferido,
+                                                despesa_2019_2,
+                                                rateio_despesa_2019_role_conferido):
+    observacoes = "Teste"
+    prestacao = concluir_prestacao_de_contas(prestacao_contas_uuid=prestacao_conta_iniciada.uuid,
+                                             observacoes=observacoes)
+    assert prestacao.fechamentos_da_prestacao.count() == 2, "Deveriam ter sido criados dois fechamentos, um por ação."
+
+
+def test_deve_sumarizar_transacoes_incluindo_nao_conferidas(prestacao_conta_iniciada,
+                                                            periodo_2020_1,
+                                                            receita_2020_1_role_repasse_custeio_conferida,
+                                                            receita_2020_1_role_repasse_capital_nao_conferida,
+                                                            receita_2019_2_role_repasse_capital_conferida,
+                                                            receita_2020_1_role_repasse_capital_conferida,
+                                                            receita_2020_1_role_rendimento_custeio_conferida,
+                                                            despesa_2020_1,
+                                                            rateio_despesa_2020_role_custeio_conferido,
+                                                            rateio_despesa_2020_role_custeio_nao_conferido,
+                                                            rateio_despesa_2020_role_capital_conferido,
+                                                            despesa_2019_2,
+                                                            rateio_despesa_2019_role_conferido,
+                                                            ):
+    observacoes = "Teste"
+    prestacao = concluir_prestacao_de_contas(prestacao_contas_uuid=prestacao_conta_iniciada.uuid,
+                                             observacoes=observacoes)
+    assert prestacao.fechamentos_da_prestacao.count() == 1, "Deveriam ter sido criado apenas um fechamento."
+
+    fechamento = prestacao.fechamentos_da_prestacao.first()
+
+    total_receitas_capital_esperado = receita_2020_1_role_repasse_capital_conferida.valor + \
+                                      receita_2020_1_role_repasse_capital_nao_conferida.valor
+    assert fechamento.total_receitas_capital == total_receitas_capital_esperado
+
+    total_repasses_capital_esperado = receita_2020_1_role_repasse_capital_conferida.valor + \
+                                      receita_2020_1_role_repasse_capital_nao_conferida.valor
+    assert fechamento.total_repasses_capital == total_repasses_capital_esperado
+
+    total_receitas_custeio_esperado = receita_2020_1_role_rendimento_custeio_conferida.valor + \
+                                      receita_2020_1_role_repasse_custeio_conferida.valor
+    assert fechamento.total_receitas_custeio == total_receitas_custeio_esperado
+
+    total_repasses_custeio_esperado = receita_2020_1_role_repasse_custeio_conferida.valor
+    assert fechamento.total_repasses_custeio == total_repasses_custeio_esperado
+
+    total_despesas_capital = rateio_despesa_2020_role_capital_conferido.valor_rateio
+    assert fechamento.total_despesas_capital == total_despesas_capital
+
+    total_despesas_custeio = rateio_despesa_2020_role_custeio_conferido.valor_rateio + \
+                             rateio_despesa_2020_role_custeio_nao_conferido.valor_rateio
+    assert fechamento.total_despesas_custeio == total_despesas_custeio
+
+
+def test_fechamentos_devem_ser_vinculados_a_anteriores(fechamento_periodo_2019_2,
+                                                       prestacao_conta_2020_1_iniciada,
+                                                       periodo_2019_2,
+                                                       periodo_2020_1,
+                                                       receita_2020_1_role_repasse_custeio_conferida,
+                                                       receita_2020_1_role_repasse_capital_nao_conferida,
+                                                       receita_2019_2_role_repasse_capital_conferida,
+                                                       receita_2020_1_role_repasse_capital_conferida,
+                                                       receita_2020_1_role_rendimento_custeio_conferida,
+                                                       despesa_2020_1,
+                                                       rateio_despesa_2020_role_custeio_conferido,
+                                                       rateio_despesa_2020_role_custeio_nao_conferido,
+                                                       rateio_despesa_2020_role_capital_conferido,
+                                                       despesa_2019_2,
+                                                       rateio_despesa_2019_role_conferido):
+    observacoes = "Teste"
+    prestacao = concluir_prestacao_de_contas(prestacao_contas_uuid=prestacao_conta_2020_1_iniciada.uuid,
+                                             observacoes=observacoes)
+
+    fechamento = prestacao.fechamentos_da_prestacao.first()
+
+    assert fechamento.fechamento_anterior == fechamento_periodo_2019_2, "Deveria apontar para o fechamento anterior."
