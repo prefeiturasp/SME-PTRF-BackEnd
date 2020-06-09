94af064c4b4c1d786533071fc9ad4e8c1361cd31
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-06-09 15:15:05 -0300

    feat(16189): Cria campo para determinar ordem das ações
    Cria na ação um capo que define a ordem da ação nas pequisas

--- sme_ptrf_apps/core/models/acao.py
+++ sme_ptrf_apps/core/models/acao.py
@@ -1,7 +1,16 @@
+from django.db import models
+
 from sme_ptrf_apps.core.models_abstracts import ModeloIdNome
 
 
 class Acao(ModeloIdNome):
+    posicao_nas_pesquisas = models.CharField(
+        'posição nas pesquisas',
+        max_length=10,
+        blank=True,
+        default='ZZZZZZZZZZ',
+        help_text='A ordem alfabética desse texto definirá a ordem que a ação será exibida nas pesquisas.'
+    )
 
     class Meta:
         verbose_name = "Ação"
