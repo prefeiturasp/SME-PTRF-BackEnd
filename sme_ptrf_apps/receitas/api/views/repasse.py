1fa3506c1ce2e5829146247ad016666e5cf0518c
anderson89marques <andersonoanjo18@gmail.com>
2020-07-07 13:06:51 -0300

    Correçao cadastro receitas repasse.

--- sme_ptrf_apps/receitas/api/views/repasse.py
+++ sme_ptrf_apps/receitas/api/views/repasse.py
@@ -8,6 +8,7 @@ from rest_framework.viewsets import GenericViewSet
 from sme_ptrf_apps.core.models import Periodo
 from ...models import Repasse, Receita
 from ..serializers import RepasseSerializer
+from sme_ptrf_apps.despesas.tipos_aplicacao_recurso import APLICACAO_CAPITAL, APLICACAO_CUSTEIO
 
 
 class RepasseViewSet(GenericViewSet):
@@ -23,7 +24,6 @@ class RepasseViewSet(GenericViewSet):
         
         data = datetime.strptime(data_str, '%d/%m/%Y')
         periodo = Periodo.da_data(data)
-
         if not receita_uuid:
             """Em conversa com o time ficou definido que não deve existir repasse
             pendente para a mesma ação associação no mesmo período.
@@ -34,26 +34,31 @@ class RepasseViewSet(GenericViewSet):
                 periodo=periodo,
                 status='PENDENTE')\
             .order_by('-criado_em').last()
+            serializer = RepasseSerializer(repasse)
+            resp = serializer.data
         else:
             try:
                 receita = Receita.objects.get(uuid=receita_uuid)
                 repasse = receita.repasse
-
-                if repasse.acao_associacao.uuid != acao_associacao_uuid:
+                if str(repasse.acao_associacao.uuid) != acao_associacao_uuid:
                     """Caso o usuário troque para uma ação associação diferente do que está vinculado a receita
                     que está sendo editada."""
+                    print("Trocou")
                     repasse = Repasse.objects\
                     .filter(
                         acao_associacao__uuid=acao_associacao_uuid, 
                         periodo=periodo,
                         status='PENDENTE')\
                     .order_by('-criado_em').last()
-
+                serializer = RepasseSerializer(repasse)
+                resp = serializer.data
+                if receita.categoria_receita == APLICACAO_CAPITAL:
+                    resp['valor_capital'] = str(repasse.valor_capital)
+                else:
+                    resp['valor_custeio'] = str(repasse.valor_custeio)
             except Receita.DoesNotExist as e:
                 return Response("Repasse não encontrado para receita {receita.uuid}.", status=HTTP_400_BAD_REQUEST)
         
         if not repasse:
             return Response(f"Repasse não encontrado para ação-associação {acao_associacao_uuid} e para o periodo {periodo}.", status=HTTP_404_NOT_FOUND)
-
-        serializer = RepasseSerializer(repasse)
-        return Response(serializer.data)
+        return Response(resp)
