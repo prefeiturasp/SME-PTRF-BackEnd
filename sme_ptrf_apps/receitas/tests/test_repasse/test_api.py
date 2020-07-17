772951b2205541595aebae8f4308e233ec5d9953
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-07-17 11:32:30 -0300

    feat(19335): Atualiza a api de repasses para considerar livre aplicação
    Altera a API de repasses para considerar os repasses de livre aplicação.

--- sme_ptrf_apps/receitas/tests/test_repasse/test_api.py
+++ sme_ptrf_apps/receitas/tests/test_repasse/test_api.py
@@ -21,6 +21,7 @@ def test_repasses_pendentes(
     esperado = {
         'valor_capital': '1000.28',
         'valor_custeio': '1000.40',
+        'valor_livre': '0.00',
         'acao_associacao': {
             'uuid': str(acao_associacao.uuid),
             'id': acao_associacao.id,
@@ -36,5 +37,40 @@ def test_repasses_pendentes(
             'data_fim_realizacao_despesas': '2019-11-30'
         }
     }
-    #assert response.status_code == HTTP_200_OK
+
+    assert result == esperado
+
+
+def test_repasses_pendentes_livre_aplicacao(
+    client,
+    periodo_2020_1,
+    repasse_2020_1_livre_aplicacao_pendente,
+    acao,
+    acao_associacao,
+    associacao,
+    tipo_conta,
+    conta_associacao):
+
+    response = client.get(f'/api/repasses/pendentes/?acao-associacao={acao_associacao.uuid}&data=02/03/2020', content_type='application/json')
+    result = json.loads(response.content)
+    esperado = {
+        'valor_capital': '0.00',
+        'valor_custeio': '0.00',
+        'valor_livre': '1000.00',
+        'acao_associacao': {
+            'uuid': str(acao_associacao.uuid),
+            'id': acao_associacao.id,
+            'nome': acao_associacao.acao.nome
+        },
+        'conta_associacao': {
+            'uuid': str(conta_associacao.uuid),
+            'nome': conta_associacao.tipo_conta.nome
+        },
+        'periodo': {
+            'uuid': str(periodo_2020_1.uuid),
+            'data_inicio_realizacao_despesas': '2020-01-01',
+            'data_fim_realizacao_despesas': '2020-06-30'
+        }
+    }
+
     assert result == esperado
