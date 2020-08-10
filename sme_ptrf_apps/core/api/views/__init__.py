9850c19d2ad2bada96ba136ff66f0987cb55b0f7
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-08-10 10:10:20 -0300

    Criando unidades_viewset, alterando unidade_serializer, criando rota para api/unidades e alterando teste test_retrieve_associacao

--- sme_ptrf_apps/core/api/views/__init__.py
+++ sme_ptrf_apps/core/api/views/__init__.py
@@ -6,3 +6,4 @@ from .periodos_viewset import PeriodosViewSet
 from .prestacoes_contas_viewset import PrestacoesContasViewSet
 from .processos_associacao_viewset import ProcessosAssociacaoViewSet
 from .relacao_bens_viewset import RelacaoBensViewSet
+from .unidades_viewset import UnidadesViewSet
