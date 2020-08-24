a4022d0151d4380d98116354e869c4a51af3f091
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2020-08-24 13:47:53 -0300

    Criando test_meta_model(faq_categoria)

--- sme_ptrf_apps/dre/tests/tests_faq_categoria/test_faq_categoria_model.py
+++ sme_ptrf_apps/dre/tests/tests_faq_categoria/test_faq_categoria_model.py
@@ -14,3 +14,8 @@ def test_instance_model(faq_categoria):
 
 def test_str_model(faq_categoria):
     assert faq_categoria.__str__() == 'Geral'
+
+
+def test_meta_model(faq_categoria):
+    assert faq_categoria._meta.verbose_name == 'Faq - Categoria'
+    assert faq_categoria._meta.verbose_name_plural == 'Faqs - Categorias'
