b283326e15737f7770c324737620f1b44bd8620c
anderson89marques <andersonoanjo18@gmail.com>
2020-09-04 16:36:17 -0300

    Atualizaçao dos dados de uma unidade a partir da API do eol e do SGP

--- sme_ptrf_apps/core/services/__init__.py
+++ sme_ptrf_apps/core/services/__init__.py
@@ -1,14 +1,17 @@
-from .conciliacao_services import (receitas_conciliadas_por_conta_e_acao_na_conciliacao, info_conciliacao_pendente,
-                                   despesas_nao_conciliadas_por_conta_e_acao_no_periodo,
-                                   despesas_conciliadas_por_conta_e_acao_na_conciliacao,
-                                   receitas_nao_conciliadas_por_conta_e_acao_no_periodo)
+from .conciliacao_services import (
+    despesas_conciliadas_por_conta_e_acao_na_conciliacao,
+    despesas_nao_conciliadas_por_conta_e_acao_no_periodo,
+    info_conciliacao_pendente,
+    receitas_conciliadas_por_conta_e_acao_na_conciliacao,
+    receitas_nao_conciliadas_por_conta_e_acao_no_periodo,
+)
 from .exporta_associacao import gerar_planilha
 from .implantacao_saldos_services import implanta_saldos_da_associacao, implantacoes_de_saldo_da_associacao
 from .info_por_acao_services import (
     info_acao_associacao_no_periodo,
     info_acoes_associacao_no_periodo,
+    info_painel_acoes_por_periodo_e_conta,
     saldos_insuficientes_para_rateios,
-    info_painel_acoes_por_periodo_e_conta
 )
 from .membro_associacao_service import TerceirizadasException, TerceirizadasService
 from .periodo_associacao_services import status_aceita_alteracoes_em_transacoes, status_periodo_associacao
@@ -19,4 +22,4 @@ from .prestacao_contas_services import (
     revisar_prestacao_de_contas,
 )
 from .processa_cargas import processa_cargas
-from .unidade_service import monta_unidade_para_atribuicao
+from .unidade_service import atualiza_dados_unidade, monta_unidade_para_atribuicao
--- sme_ptrf_apps/core/services/unidade_service.py
+++ sme_ptrf_apps/core/services/unidade_service.py
@@ -1,6 +1,12 @@
+import requests
+import logging
+
+from django.conf import settings
 from sme_ptrf_apps.core.models import Unidade
 from sme_ptrf_apps.dre.models import Atribuicao
 from sme_ptrf_apps.dre.api.serializers.tecnico_dre_serializer import TecnicoDreLookUpSerializer
+
+logger = logging.getLogger(__name__)
 
 
 def monta_unidade_para_atribuicao(queryset, dre_uuid, periodo):
@@ -19,3 +25,64 @@ def monta_unidade_para_atribuicao(queryset, dre_uuid, periodo):
         list_unidades.append(d)
 
     return list_unidades
+
+
+def atualiza_dados_unidade(associacao) -> None:
+    """
+    Responsável por atualizar os dados da unidade vinculada a associação
+    recebida como parâmetro.
+    Os dados são consultados de duas APIs: API do eol e API do SGP.
+    """
+    unidade: Unidade = associacao.unidade
+    logger.info("Atualizando dados da unidade %s", str(unidade))
+
+    atualiza_dados_pessoais_unidade(unidade)
+    atualiza_diretor_unidade(unidade)
+    logger.info("Dados da unidade %s atualizados com sucesso.", str(unidade))
+
+
+def atualiza_dados_pessoais_unidade(unidade: Unidade) -> None:
+    """Consulta informações da unidade na API do eol"""
+    headers_eol = {
+        "Authorization": f"Token {settings.EOL_API_TERCEIRIZADAS_TOKEN}"
+    }
+    timeout = 10
+
+    try:
+        response = requests.get(f'{settings.EOL_API_TERCEIRIZADAS_URL}/escolas_terceirizadas/',
+                                headers=headers_eol, timeout=timeout)
+        results = response.json()['results']
+        resultado = list(filter(lambda d: d['cd_unidade_educacao'] == unidade.codigo_eol, results))
+        if resultado:
+            unidade_retorno = resultado[0]
+            unidade.nome = unidade_retorno['nm_unidade_educacao']
+            unidade.email = unidade_retorno['email']
+            unidade.telefone = unidade_retorno['tel1']
+            unidade.numero = unidade_retorno['cd_nr_endereco']
+            unidade.tipo_logradouro = unidade_retorno['dc_tp_logradouro']
+            unidade.logradouro = unidade_retorno['nm_logradouro']
+            unidade.bairro = unidade_retorno['nm_bairro']
+            unidade.cep = f"{unidade_retorno['cd_cep']:0>8}"
+            unidade.save()
+    except Exception as err:
+        logger.info("Erro ao Atualizar dados pessoais da unidade %s", err)
+
+
+def atualiza_diretor_unidade(unidade: Unidade) -> None:
+    """Consulta informação do diretor da unidade na API do sgp"""
+    headers_sgp = {
+        "x-api-eol-key": f"{settings.SME_INTEGRACAO_TOKEN}"
+    }
+
+    timeout = 10
+    codigo_cargo = '3360'
+
+    try:
+        response = requests.get(f'{settings.SME_INTEGRACAO_URL}/api/escolas/{unidade.codigo_eol}/funcionarios/cargos/{codigo_cargo}',
+                                headers=headers_sgp, timeout=timeout)
+        resultado = response.json()
+        if resultado:
+            unidade.diretor_nome = resultado[0]['nomeServidor']
+        logger.info(response.json())
+    except Exception as err:
+        logger.info("Erro ao atualizar diretor: %s", err)
