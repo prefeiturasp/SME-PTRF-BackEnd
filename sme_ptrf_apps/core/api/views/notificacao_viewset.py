7ee729868a40fcb115e7480471ea571e54dc4769
anderson89marques <andersonoanjo18@gmail.com>
2020-09-11 14:42:34 -0300

    Adiçao da paginaçao na listagem da notificaçao

--- sme_ptrf_apps/core/api/views/notificacao_viewset.py
+++ sme_ptrf_apps/core/api/views/notificacao_viewset.py
@@ -2,6 +2,7 @@ import logging
 
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -12,15 +13,37 @@ from sme_ptrf_apps.core.services import formata_data
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_PAGE = 1
+DEFAULT_PAGE_SIZE = 10
+
+
+class CustomPagination(PageNumberPagination):
+    page = DEFAULT_PAGE
+    page_size = DEFAULT_PAGE_SIZE
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'page': int(self.request.GET.get('page', DEFAULT_PAGE)),
+            'page_size': int(self.request.GET.get('page_size', self.page_size)),
+            'results': data
+        })
+
 
 class NotificacaoViewSet(viewsets.ModelViewSet):
     lookup_field = "uuid"
     permission_classes = (IsAuthenticated,)
     queryset = Notificacao.objects.all()
     serializer_class = NotificacaoSerializer
+    pagination_class = CustomPagination
 
     def get_queryset(self):
-        qs = Notificacao.objects.all()
+        qs = Notificacao.objects.all().order_by("-criado_em")
 
         if self.request.query_params.get('lido'):
             qs = qs.filter(lido=(self.request.query_params.get('lido') == 'True'))
@@ -36,21 +59,37 @@ class NotificacaoViewSet(viewsets.ModelViewSet):
 
         data_inicio = self.request.query_params.get('data_inicio')
         data_fim = self.request.query_params.get('data_fim')
+
         if data_inicio is not None and data_fim is not None:
             qs = qs.filter(criado_em__range=[data_inicio, data_fim])
 
         return qs
 
     def list(self, request, *args, **kwargs):
-        datas = sorted(self.get_queryset().dates("criado_em", "day"), reverse=True)
-        lista = []
-        for data in datas:
-            d = {"data": formata_data(data), "infos": NotificacaoSerializer(self.get_queryset().filter(criado_em__year=data.year,
-                                                                                                       criado_em__month=data.month,
-                                                                                                       criado_em__day=data.day), many=True).data}
-            lista.append(d)
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
 
-        return Response(lista)
+        lista = []
+        result = None
+        if page is not None:
+            datas = sorted(set([p.criado_em.date() for p in page]), reverse=True)
+            for data in datas:
+                d = {"data": formata_data(data), "infos": NotificacaoSerializer([n for n in page if (
+                    n.criado_em.year == data.year and n.criado_em.month == data.month and n.criado_em.day == data.day)], many=True).data}
+                lista.append(d)
+
+            result = self.get_paginated_response(lista).data
+        else:
+            datas = sorted(self.get_queryset().dates("criado_em", "day"), reverse=True)
+
+            for data in datas:
+                d = {"data": formata_data(data), "infos": NotificacaoSerializer(self.get_queryset().filter(criado_em__year=data.year,
+                                                                                                           criado_em__month=data.month,
+                                                                                                           criado_em__day=data.day), many=True).data}
+                lista.append(d)
+            result = lista
+
+        return Response(result)
 
     @action(detail=False, methods=['get'], url_path='quantidade-nao-lidos')
     def quantidade_de_nao_lidos(self, request):
