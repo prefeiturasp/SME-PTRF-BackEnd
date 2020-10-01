1365f9ed7a97f16bdc7069ada33687f4f6235d51
anderson89marques <andersonoanjo18@gmail.com>
2020-10-01 12:53:31 -0300

    Modelo nao permitir criar um membro ja cadastrado na base.

--- sme_ptrf_apps/core/tests/tests_membro_associacao/test_membro_associacao_model.py
+++ sme_ptrf_apps/core/tests/tests_membro_associacao/test_membro_associacao_model.py
@@ -18,6 +18,16 @@ def test_instance_model(membro_associacao):
     assert model.email
 
 
+def test_nao_criar_novo_membro_caso_exista_pelo_nome(membro_associacao):
+    with pytest.raises(ValueError):
+        MembroAssociacao.objects.create(nome=membro_associacao.nome)
+
+
+def test_nao_criar_novo_membro_caso_exista_pelo_codigo_identificaca0(membro_associacao):
+    with pytest.raises(ValueError):
+        MembroAssociacao.objects.create(codigo_identificacao=membro_associacao.codigo_identificacao)
+
+
 def test_admin():
     from django.contrib import admin
     assert admin.site._registry[MembroAssociacao]
