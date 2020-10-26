5dc9c570177e79c92919c87ea4c9ab867e0ff749
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-10-26 10:21:28 -0300

    feat(23669): Informações Ata de retificação da PC
    Acrescenta no serializer da ata de retificação
    as informações específicas desse tipo de ata.

--- sme_ptrf_apps/core/tests/tests_api_atas_associacao/test_retrieve_ata_associacao.py
+++ sme_ptrf_apps/core/tests/tests_api_atas_associacao/test_retrieve_ata_associacao.py
@@ -33,7 +33,42 @@ def test_api_retrieve_ata_associacao(client, associacao, ata_2020_1_cheque_aprov
         'secretario_reuniao': 'Ana',
         'tipo_ata': 'APRESENTACAO',
         'tipo_reuniao': 'ORDINARIA',
-        'uuid': f'{ata_2020_1_cheque_aprovada.uuid}'
+        'uuid': f'{ata_2020_1_cheque_aprovada.uuid}',
+        'retificacoes': ''
+    }
+    assert response.status_code == status.HTTP_200_OK
+    assert result == result_esperado
+
+
+def test_api_retrieve_ata_retificacao_associacao(client, associacao, ata_2020_1_retificacao):
+    response = client.get(f'/api/atas-associacao/{ata_2020_1_retificacao.uuid}/', content_type='application/json')
+    result = json.loads(response.content)
+
+    result_esperado = {
+        'associacao': {'cnpj': '52.302.275/0001-83',
+                       'nome': 'Escola Teste',
+                       'unidade': {'nome': 'Escola Teste', 'tipo_unidade': 'CEU'},
+                       'uuid': f'{ata_2020_1_retificacao.associacao.uuid}'},
+        'cargo_presidente_reuniao': 'Presidente',
+        'cargo_secretaria_reuniao': 'Secretária',
+        'comentarios': 'Teste',
+        'convocacao': 'PRIMEIRA',
+        'data_reuniao': '2020-07-01',
+        'local_reuniao': 'Escola Teste',
+        'nome': 'Ata de Retificação da prestação de contas',
+        'parecer_conselho': 'APROVADA',
+        'periodo': {'data_fim_realizacao_despesas': '2020-06-30',
+                    'data_inicio_realizacao_despesas': '2020-01-01',
+                    'referencia': '2020.1',
+                    'referencia_por_extenso': '1° repasse de 2020',
+                    'uuid': f'{ata_2020_1_retificacao.periodo.uuid}'},
+        'presidente_reuniao': 'José',
+        'prestacao_conta': f'{ata_2020_1_retificacao.prestacao_conta.uuid}',
+        'secretario_reuniao': 'Ana',
+        'tipo_ata': 'RETIFICACAO',
+        'tipo_reuniao': 'ORDINARIA',
+        'uuid': f'{ata_2020_1_retificacao.uuid}',
+        'retificacoes': 'Teste'
     }
     assert response.status_code == status.HTTP_200_OK
     assert result == result_esperado
