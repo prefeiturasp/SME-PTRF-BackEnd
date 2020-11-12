3842cdb0077efee8ded0aa3d1de2b1140b39b21a
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-11-12 07:28:12 -0300

    feat(26292): Filtro por fornecedor em consulta de totais de rateio
    Testa a aplicação de filtro por fornecedor na consulta de totais de rateio.

--- sme_ptrf_apps/despesas/tests/tests_api_rateios_despesas/test_get_rateios_despesas_totais.py
+++ sme_ptrf_apps/despesas/tests/tests_api_rateios_despesas/test_get_rateios_despesas_totais.py
@@ -207,3 +207,72 @@ def test_api_get_despesas_totais_por_periodo(jwt_authenticated_client_d, associa
 
     assert response.status_code == status.HTTP_200_OK
     assert result == esperado
+
+
+@pytest.fixture
+def despesa_fornecedor_xpto(associacao, tipo_documento, tipo_transacao):
+    return baker.make(
+        'Despesa',
+        associacao=associacao,
+        numero_documento='9999999',
+        data_documento=date(2020, 3, 11),
+        tipo_documento=tipo_documento,
+        cpf_cnpj_fornecedor='11.478.276/0001-00',
+        nome_fornecedor='XPTO',
+        tipo_transacao=tipo_transacao,
+        data_transacao=date(2020, 3, 10),
+        valor_total=100.00,
+    )
+
+
+@pytest.fixture
+def rateio_despesa_fornecedor_xpto(associacao, despesa_fornecedor_xpto, conta_associacao, acao,
+                                   tipo_aplicacao_recurso_custeio,
+                                   tipo_custeio_material,
+                                   especificacao_material_eletrico, acao_associacao_role_cultural):
+    return baker.make(
+        'RateioDespesa',
+        despesa=despesa_fornecedor_xpto,
+        associacao=associacao,
+        conta_associacao=conta_associacao,
+        acao_associacao=acao_associacao_role_cultural,
+        aplicacao_recurso=tipo_aplicacao_recurso_custeio,
+        tipo_custeio=tipo_custeio_material,
+        especificacao_material_servico=especificacao_material_eletrico,
+        valor_rateio=777.00,
+
+    )
+
+
+def test_api_get_despesas_totais_por_fornecedor(jwt_authenticated_client_d, associacao, despesa,
+                                                conta_associacao, acao,
+                                                tipo_aplicacao_recurso_custeio,
+                                                tipo_custeio_servico,
+                                                especificacao_instalacao_eletrica, acao_associacao,
+                                                especificacao_material_eletrico,
+                                                rateio_despesa_material_eletrico_role_cultural,
+                                                rateio_despesa_instalacao_eletrica_ptrf,
+                                                despesa_fornecedor_xpto,
+                                                rateio_despesa_fornecedor_xpto
+                                                ):
+    response = jwt_authenticated_client_d.get(
+        f'/api/rateios-despesas/totais/?associacao__uuid={associacao.uuid}&fornecedor=XPTO',
+        content_type='application/json')
+    result = json.loads(response.content)
+
+    total_despesas_sem_filtro = rateio_despesa_material_eletrico_role_cultural.valor_rateio + \
+                                rateio_despesa_instalacao_eletrica_ptrf.valor_rateio + \
+                                rateio_despesa_fornecedor_xpto.valor_rateio
+
+    total_despesas_com_filtro = rateio_despesa_fornecedor_xpto.valor_rateio
+
+    results = {
+        "associacao_uuid": f'{associacao.uuid}',
+        "total_despesas_sem_filtro": total_despesas_sem_filtro,
+        "total_despesas_com_filtro": total_despesas_com_filtro
+    }
+
+    esperado = results
+
+    assert response.status_code == status.HTTP_200_OK
+    assert result == esperado
