3a16704e2645e62df10ad73cefceb7a320a74569
anderson89marques <andersonoanjo18@gmail.com>
2020-11-13 19:13:51 -0300

    Hotfix nas cargas de arquivos do sistema.

--- sme_ptrf_apps/users/services/carga_usuarios.py
+++ sme_ptrf_apps/users/services/carga_usuarios.py
@@ -86,16 +86,21 @@ def carrega_usuarios(arquivo):
     logger.info('Carregando arquivo de usuários.')
     arquivo.ultima_execucao = datetime.datetime.now()
 
-    with open(arquivo.conteudo.path, 'r', encoding="utf-8") as f:
-        sniffer = csv.Sniffer().sniff(f.read(1024))
-        f.seek(0)
-        if  __DELIMITADORES[sniffer.delimiter] != arquivo.tipo_delimitador:
-            msg_erro = f"Formato definido ({arquivo.tipo_delimitador}) é diferente do formato do arquivo csv ({__DELIMITADORES[sniffer.delimiter]})"
-            logger.info(msg_erro)
-            arquivo.status = ERRO
-            arquivo.log = msg_erro
-            arquivo.save()
-            return
+    try:
+        with open(arquivo.conteudo.path, 'r', encoding="utf-8") as f:
+            sniffer = csv.Sniffer().sniff(f.readline())
+            f.seek(0)
+            if  __DELIMITADORES[sniffer.delimiter] != arquivo.tipo_delimitador:
+                msg_erro = f"Formato definido ({arquivo.tipo_delimitador}) é diferente do formato do arquivo csv ({__DELIMITADORES[sniffer.delimiter]})"
+                logger.info(msg_erro)
+                arquivo.status = ERRO
+                arquivo.log = msg_erro
+                arquivo.save()
+                return
 
-        reader = csv.reader(f, delimiter=sniffer.delimiter)
-        processa_importacao_usuarios(reader, arquivo)
+            reader = csv.reader(f, delimiter=sniffer.delimiter)
+            processa_importacao_usuarios(reader, arquivo)
+    except Exception as err:
+        logger.info("Erro ao processar usuários: %s", str(err))
+        arquivo.log = "Erro ao processar usuários."
+        arquivo.save()
