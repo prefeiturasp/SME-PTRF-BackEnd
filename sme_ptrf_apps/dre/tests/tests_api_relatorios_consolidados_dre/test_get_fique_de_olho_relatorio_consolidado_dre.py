2f32c75fb33e59e439f10a0b602d8ac9d5e2cef5
Alessandro Fernandes <alcfernandes@yahoo.com>
2020-12-29 10:41:32 -0300

    feat(29736): Altera API fique-de-olho rel DRE
    Alterar a API de recuperação do texto do fique de olho
    de relatórios DRE para considerar os parâmetros separados.

--- sme_ptrf_apps/dre/tests/tests_api_relatorios_consolidados_dre/test_get_fique_de_olho_relatorio_consolidado_dre.py
+++ sme_ptrf_apps/dre/tests/tests_api_relatorios_consolidados_dre/test_get_fique_de_olho_relatorio_consolidado_dre.py
@@ -1,20 +1,29 @@
 import json
 
 import pytest
+from model_bakery import baker
 
 from rest_framework import status
 
 pytestmark = pytest.mark.django_db
 
 
-def test_api_get_status_relatorio_analise_completa_rel_nao_gerado(jwt_authenticated_client_relatorio_consolidado, parametros):
+@pytest.fixture
+def parametro_fique_de_olho_rel_dre_abc():
+    return baker.make(
+        'ParametroFiqueDeOlhoRelDre',
+        fique_de_olho='abc',
+    )
+
+
+def test_api_get_status_relatorio_analise_completa_rel_nao_gerado(jwt_authenticated_client_relatorio_consolidado,
+                                                                  parametro_fique_de_olho_rel_dre_abc):
     response = jwt_authenticated_client_relatorio_consolidado.get(
         f'/api/relatorios-consolidados-dre/fique-de-olho/',
         content_type='application/json')
     result = json.loads(response.content)
 
-    resultado_esperado = {'detail': ''}
+    resultado_esperado = {'detail': 'abc'}
 
     assert response.status_code == status.HTTP_200_OK
     assert result == resultado_esperado
-
