bc54b7a711ac2c12bce24a99c04d865ff1649cd9
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-01-13 07:50:32 -0300

    feat(29897): List ações associações filtros
    Endpoint para listar todas as ações de associações
    com filtros por nome de associação e escola e
    código eol.

--- sme_ptrf_apps/core/api/views/acao_associacao_viewset.py
+++ sme_ptrf_apps/core/api/views/acao_associacao_viewset.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
@@ -17,6 +19,17 @@ class AcaoAssociacaoViewSet(mixins.RetrieveModelMixin,
     permission_classes = [IsAuthenticated & PermissaoAssociacao]
     serializer_class = AcaoAssociacaoRetrieveSerializer
     queryset = AcaoAssociacao.objects.all().order_by('associacao__nome', 'acao__nome')
+
+    def get_queryset(self):
+        qs = AcaoAssociacao.objects.all()
+
+        nome = self.request.query_params.get('nome')
+        if nome is not None:
+            qs = qs.filter(Q(associacao__nome__unaccent__icontains=nome) | Q(
+                associacao__unidade__nome__unaccent__icontains=nome) | Q(
+                associacao__unidade__codigo_eol__icontains=nome))
+
+        return qs.order_by('associacao__nome', 'acao__nome')
 
     def get_serializer_class(self):
         if self.action in ['retrieve', 'list']:
--- sme_ptrf_apps/core/tests/tests_api_acoes_associacoes/conftest.py
+++ sme_ptrf_apps/core/tests/tests_api_acoes_associacoes/conftest.py
@@ -25,7 +25,7 @@ def dre_alfa():
 
 
 @pytest.fixture
-def unidade_bravo(dre_alfa):
+def unidade_bravo_000086(dre_alfa):
     return baker.make(
         'Unidade',
         codigo_eol="000086",
@@ -37,36 +37,36 @@ def unidade_bravo(dre_alfa):
 
 
 @pytest.fixture
-def associacao_charli(unidade_bravo, periodo_anterior):
+def associacao_charli_bravo_000086(unidade_bravo_000086, periodo_anterior):
     return baker.make(
         'Associacao',
         nome='Charli',
         cnpj='52.302.275/0001-83',
-        unidade=unidade_bravo,
+        unidade=unidade_bravo_000086,
         periodo_inicial=periodo_anterior,
     )
 
 
 @pytest.fixture
-def acao_associacao_charli_x(associacao_charli, acao_x):
+def acao_associacao_charli_bravo_000086_x(associacao_charli_bravo_000086, acao_x):
     return baker.make(
         'AcaoAssociacao',
-        associacao=associacao_charli,
+        associacao=associacao_charli_bravo_000086,
         acao=acao_x
     )
 
 
 @pytest.fixture
-def acao_associacao_charli_y(associacao_charli, acao_y):
+def acao_associacao_charli_bravo_000086_y(associacao_charli_bravo_000086, acao_y):
     return baker.make(
         'AcaoAssociacao',
-        associacao=associacao_charli,
+        associacao=associacao_charli_bravo_000086,
         acao=acao_y
     )
 
 
 @pytest.fixture
-def unidade_delta(dre_alfa):
+def unidade_delta_000087(dre_alfa):
     return baker.make(
         'Unidade',
         codigo_eol="000087",
@@ -78,20 +78,20 @@ def unidade_delta(dre_alfa):
 
 
 @pytest.fixture
-def associacao_eco(unidade_delta, periodo_anterior):
+def associacao_eco_delta_000087(unidade_delta_000087, periodo_anterior):
     return baker.make(
         'Associacao',
         nome='Eco',
         cnpj='94.175.194/0001-00',
-        unidade=unidade_delta,
+        unidade=unidade_delta_000087,
         periodo_inicial=periodo_anterior,
     )
 
 
 @pytest.fixture
-def acao_associacao_eco_x(associacao_eco, acao_x):
+def acao_associacao_eco_delta_000087_x(associacao_eco_delta_000087, acao_x):
     return baker.make(
         'AcaoAssociacao',
-        associacao=associacao_eco,
+        associacao=associacao_eco_delta_000087,
         acao=acao_x
     )
