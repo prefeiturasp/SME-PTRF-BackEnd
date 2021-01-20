a642d294454c6112b6b65666110ee6c5af1e5300
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-01-20 10:16:47 -0300

    feat(29908): Tratamento de deleção de período
    No caso de tentar-se excluir um período que é referenciado
    no sistema ira retornar um 400 com a mensagem de erro.

--- sme_ptrf_apps/core/api/views/periodos_viewset.py
+++ sme_ptrf_apps/core/api/views/periodos_viewset.py
@@ -40,6 +40,22 @@ class PeriodosViewSet(mixins.ListModelMixin,
         else:
             return PeriodoSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        from django.db.models.deletion import ProtectedError
+
+        obj = self.get_object()
+
+        try:
+            self.perform_destroy(obj)
+        except ProtectedError:
+            content = {
+                'erro': 'ProtectedError',
+                'mensagem': 'Esse período não pode ser excluido porque está sendo usado na aplicação.'
+            }
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
     @action(detail=False)
     def lookup(self, _):
         return Response(PeriodoLookUpSerializer(self.queryset.order_by('-referencia'), many=True).data)
