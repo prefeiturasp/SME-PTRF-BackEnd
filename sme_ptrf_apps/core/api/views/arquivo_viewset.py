ff4423185d20f4ce37ca238f67a23c6d008ab841
anderson89marques <andersonoanjo18@gmail.com>
2021-01-21 03:48:47 -0300

    Download Arquivo de carga.

--- sme_ptrf_apps/core/api/views/arquivo_viewset.py
+++ sme_ptrf_apps/core/api/views/arquivo_viewset.py
@@ -1,5 +1,9 @@
+import logging
+
 from django.db.models import Q
+from django.http import HttpResponse
 from django_filters import rest_framework as filters
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
@@ -8,6 +12,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from sme_ptrf_apps.core.api.serializers import ArquivoSerializer
 from sme_ptrf_apps.core.models import Arquivo
+
+logger = logging.getLogger(__name__)
 
 
 class ArquivoViewSet(ModelViewSet):
@@ -31,8 +37,7 @@ class ArquivoViewSet(ModelViewSet):
 
         return qs
 
-
-    @action(detail=False, url_path='tabelas')
+    @action(detail=False, methods=['get'], url_path='tabelas')
     def tabelas(self, _):
         result = {
             'status': Arquivo.status_to_json(),
@@ -40,3 +45,42 @@ class ArquivoViewSet(ModelViewSet):
             'tipos_delimitadores': Arquivo.delimitadores_to_json()
         }
         return Response(result)
+
+    @action(detail=False, methods=['get'], url_path='download')
+    def download(self, request):
+        uuid = request.query_params.get('uuid')
+        logger.info("Download do arquivo de carga com uuid %s.", uuid)
+
+        if not uuid:
+            erro = {
+                'erro': 'parametros_requeridos',
+                'mensagem': 'É necessário enviar o uuid do Arquivo.'
+            }
+            return Response(erro, status=status.HTTP_400_BAD_REQUEST)
+
+        arquivo = Arquivo.objects.filter(uuid=uuid).first()
+        if not arquivo:
+            logger.info("Arquivo com uuid %s não encontrado.", uuid)
+            erro = {
+                'erro': 'arquivo_nao_encontrado',
+                'mensagem': f"Arquivo com uuid {uuid} não encontrado."
+            }
+            return Response(erro, status=status.HTTP_404_NOT_FOUND)
+
+        logger.info("Retornando dados do arquivo: %s", arquivo.conteudo.path)
+        filename = arquivo.conteudo.name
+        try:
+            response = HttpResponse(
+                open(arquivo.conteudo.path, 'rb'),
+                content_type='text/csv'
+            )
+            response['Content-Disposition'] = 'attachment; filename=%s' % filename
+        except Exception as err:
+            erro = {
+                'erro': 'arquivo_nao_gerado',
+                'mensagem': str(err)
+            }
+            logger.info("Erro: %s", str(err))
+            return Response(erro, status=status.HTTP_404_NOT_FOUND)
+
+        return response
