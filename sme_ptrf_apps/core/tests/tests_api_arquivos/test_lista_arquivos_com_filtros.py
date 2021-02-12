169376966a225153cf502816ee0a706098662f5c
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-02-12 07:06:33 -0300

    feat(29892): Ajusta filtro de arquivos por data
    Corrige o filtre de arquivos por data que não estava retornando os
    resultados corretos.

--- sme_ptrf_apps/core/tests/tests_api_arquivos/test_lista_arquivos_com_filtros.py
+++ sme_ptrf_apps/core/tests/tests_api_arquivos/test_lista_arquivos_com_filtros.py
@@ -74,23 +74,25 @@ def test_lista_arquivos_por_status(jwt_authenticated_client, arquivo_carga):
     assert result == resulta_esperado
 
 
-def test_lista_arquivos_por_data_execucao(jwt_authenticated_client, arquivo_carga):
-    response = jwt_authenticated_client.get('/api/arquivos/?data_execucao=2021-01-21')
+def test_lista_arquivos_por_data_execucao(jwt_authenticated_client, arquivo_carga_associacao_processado_2020_01_01):
+    tipo_carga = "CARGA_ASSOCIACOES"
+    response = jwt_authenticated_client.get(f'/api/arquivos/?tipo_carga={tipo_carga}&data_execucao=2020-01-01')
     result = response.json()
 
     resulta_esperado = [
         {
-            'id': arquivo_carga.id,
-            'criado_em': arquivo_carga.criado_em.isoformat("T"),
-            'alterado_em': arquivo_carga.alterado_em.isoformat("T"),
-            'uuid': str(arquivo_carga.uuid),
-            'identificador': 'carga_previsao_repasse',
+            'id': arquivo_carga_associacao_processado_2020_01_01.id,
+            'criado_em': arquivo_carga_associacao_processado_2020_01_01.criado_em.isoformat("T"),
+            'alterado_em': arquivo_carga_associacao_processado_2020_01_01.alterado_em.isoformat("T"),
+            'uuid': str(arquivo_carga_associacao_processado_2020_01_01.uuid),
+            'identificador': 'carga_associacao',
             'conteudo': 'http://testserver/media/arquivo.csv',
-            'tipo_carga': 'CARGA_REPASSE_PREVISTO_SME',
+            'tipo_carga': tipo_carga,
             'tipo_delimitador': 'DELIMITADOR_PONTO_VIRGULA',
             'status': 'PENDENTE',
             'log': None,
-            'ultima_execucao': arquivo_carga.ultima_execucao.isoformat("T")
+            'ultima_execucao': arquivo_carga_associacao_processado_2020_01_01.ultima_execucao.isoformat("T")
         }]
 
     assert result == resulta_esperado
+
