cca2fa4abab8e5c0b233cae8b3830c91bd8bb9d4
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-02-22 13:46:23 -0300

    feat(34030): Altera api resumo conciliação
    O resumo de conciliação passa a ser por conta apenas
    e inclui informações de saldo anterior e posterior.

--- sme_ptrf_apps/core/services/__init__.py
+++ sme_ptrf_apps/core/services/__init__.py
@@ -4,6 +4,7 @@ from .conciliacao_services import (
     info_conciliacao_pendente,
     receitas_conciliadas_por_conta_e_acao_na_conciliacao,
     receitas_nao_conciliadas_por_conta_e_acao_no_periodo,
+    info_resumo_conciliacao,
 )
 from .exporta_associacao import gerar_planilha
 from .implantacao_saldos_services import implanta_saldos_da_associacao, implantacoes_de_saldo_da_associacao
