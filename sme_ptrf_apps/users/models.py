2cf1d9ce24a9c32bead3adadb1c7d3ad0f23e032
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-03-03 08:24:27 -0300

    feat(31471): Carga usuarios atualizando grupos
    Altera a carga de usuários para atualizar os grupos de acesso.

--- sme_ptrf_apps/users/models.py
+++ sme_ptrf_apps/users/models.py
@@ -8,7 +8,7 @@ from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from sme_ptrf_apps.core.choices import RepresentacaoCargo
-from sme_ptrf_apps.core.models import Associacao, Unidade
+from sme_ptrf_apps.core.models import Unidade
 from sme_ptrf_apps.core.models_abstracts import ModeloIdNome
 
 
@@ -26,11 +26,16 @@ class Grupo(Group):
     descricao = models.TextField(blank=True, default='')
     visoes = models.ManyToManyField(Visao, blank=True)
 
+    @classmethod
+    def grupo_por_nome(cls, nome):
+        q = cls.objects.filter(nome=nome)
+        return q.first() if q.exists() else None
+
 
 class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, null=True)
     name = CharField(_("Nome do usuário"), blank=True, max_length=255)
-    hash_redefinicao = models.TextField(blank=True, default='', 
+    hash_redefinicao = models.TextField(blank=True, default='',
                                         help_text='Campo utilizado para registrar hash na redefinição de senhas.')
 
     unidades = models.ManyToManyField(Unidade, blank=True)
@@ -53,7 +58,6 @@ class User(AbstractUser):
         choices=RepresentacaoCargo.choices(),
         default=RepresentacaoCargo.SERVIDOR.value
     )
-
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
