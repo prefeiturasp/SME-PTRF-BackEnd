02898fb6bf82b41651055461b50c385e5d8f4ba1
ollyvergithub <ollyverottoboni@gmail.com>
2021-04-16 13:44:34 -0300

    Corrigindo erro decimal.InvalidOperation em TemplateTag formata_valores

--- sme_ptrf_apps/core/templatetags/formata_valores.py
+++ sme_ptrf_apps/core/templatetags/formata_valores.py
@@ -1,13 +1,23 @@
+import decimal
+
 from django import template
 
+import logging
+
 register = template.Library()
+
+LOGGER = logging.getLogger(__name__)
 
 
 @register.filter(name='formata_valor')
 def formata_valor(valor):
     if valor:
-        from babel.numbers import format_currency
-        sinal, valor_formatado = format_currency(valor, 'BRL', locale='pt_BR').split('\xa0')
-        sinal = '-' if '-' in sinal else ''
-        return f'{sinal}{valor_formatado}'
+        try:
+            from babel.numbers import format_currency
+            sinal, valor_formatado = format_currency(valor, 'BRL', locale='pt_BR').split('\xa0')
+            sinal = '-' if '-' in sinal else ''
+            return f'{sinal}{valor_formatado}'
+        except decimal.InvalidOperation as err:
+            LOGGER.error(f'Erro ao converter o valor: {valor} -  {err}')
+
     return "0,00"
