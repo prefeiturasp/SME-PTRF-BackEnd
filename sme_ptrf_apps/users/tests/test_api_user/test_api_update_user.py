6c7a0c3099c9702807a3166c220b684b128cb889
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-05-04 17:05:11 -0300

    feat(37456): Altera put user para não exigir unidade
    O put de usuário não pode exigir unidade, uma vez que
    na visão de SME não há unidade.

--- sme_ptrf_apps/users/tests/test_api_user/test_api_update_user.py
+++ sme_ptrf_apps/users/tests/test_api_user/test_api_update_user.py
@@ -67,3 +67,65 @@ def test_atualizar_usuario_servidor(
         nome='Usuario 2'
     )
     mock_atribuir_perfil_core_sso.assert_called_once_with(login='7211981', visao='UE')
+
+
+def test_atualizar_usuario_servidor_visao_sme(
+        jwt_authenticated_client_u,
+        usuario_3,
+        usuario_2,
+        visao_ue,
+        visao_dre,
+        visao_sme,
+        grupo_1,
+        grupo_2,
+        unidade_ue_271170,
+        unidade_diferente,
+        dre
+):
+
+    assert not usuario_3.visoes.filter(nome='SME').first(), "Não deveria estar vinculado à SME antes do teste."
+
+    payload = {
+        'e_servidor': True,
+        'username': usuario_3.username,
+        'name': usuario_3.name,
+        'email': 'novoEmail@gmail.com',
+        'visao': "SME",
+        'unidade': None,
+        'groups': [
+            grupo_1.id
+        ]
+    }
+
+    api_usuario_core_sso_or_none = 'sme_ptrf_apps.users.api.views.user.SmeIntegracaoService.usuario_core_sso_or_none'
+    api_cria_usuario_core_sso = 'sme_ptrf_apps.users.api.views.user.SmeIntegracaoService.cria_usuario_core_sso'
+    api_atribuir_perfil_core_sso = 'sme_ptrf_apps.users.api.views.user.SmeIntegracaoService.atribuir_perfil_coresso'
+    with patch(api_usuario_core_sso_or_none) as mock_usuario_core_sso_or_none:
+        mock_usuario_core_sso_or_none.return_value = None
+        with patch(api_cria_usuario_core_sso) as mock_cria_usuario_core_sso:
+            with patch(api_atribuir_perfil_core_sso) as mock_atribuir_perfil_core_sso:
+                response = jwt_authenticated_client_u.put(
+                    f"/api/usuarios/{usuario_3.id}/", data=json.dumps(payload), content_type='application/json')
+
+    result = response.json()
+
+    esperado = {
+        'username': usuario_3.username,
+        'email': 'novoEmail@gmail.com',
+        'name': 'Arthur Marques',
+        'e_servidor': True,
+        'groups': [grupo_1.id]
+    }
+
+    assert usuario_3.visoes.filter(nome='SME').first(), "Deveria ter sido vinculado à visão SME."
+    assert result == esperado
+
+    mock_usuario_core_sso_or_none.assert_called_once_with(login='7218198')
+    mock_cria_usuario_core_sso.assert_called_once_with(
+        e_servidor=True,
+        email='novoEmail@gmail.com',
+        login='7218198',
+        nome='Arthur Marques'
+    )
+
+    mock_atribuir_perfil_core_sso.assert_called_once_with(login=usuario_3.username, visao='SME')
