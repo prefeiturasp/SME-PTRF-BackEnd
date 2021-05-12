16aa106a94e9dc268e76d8c117e85515f0104faa
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-05-12 13:50:22 -0300

    test(39113): ADD testes para verificar gravação de usuarios sem unidade
    Cria testes para garantir ser possível criar e atualizar um usuário sem
    definir uma unidade.

--- sme_ptrf_apps/users/tests/test_api_user/test_api_create_user.py
+++ sme_ptrf_apps/users/tests/test_api_user/test_api_create_user.py
@@ -205,3 +205,57 @@ def test_criar_usuario_servidor_visao_sme(
         nome='Lukaku Silva'
     )
     mock_atribuir_perfil_core_sso.assert_has_calls([call(login='9876543', visao='UE'), call(login='9876543', visao='SME')])
+
+
+def test_criar_usuario_servidor_com_visoes_sem_definir_unidade(
+        jwt_authenticated_client_u,
+        grupo_1,
+        visao_ue,
+        unidade_ue_271170,
+):
+    payload = {
+        'e_servidor': True,
+        'username': "9876543",
+        'name': "Lukaku Silva",
+        'email': 'lukaku@gmail.com',
+        'visoes': [visao_ue.id, ],
+        'groups': [grupo_1.id, ],
+        'unidade': None
+    }
+
+    api_usuario_core_sso_or_none = 'sme_ptrf_apps.users.api.views.user.SmeIntegracaoService.usuario_core_sso_or_none'
+    api_cria_usuario_core_sso = 'sme_ptrf_apps.users.api.views.user.SmeIntegracaoService.cria_usuario_core_sso'
+    api_atribuir_perfil_core_sso = 'sme_ptrf_apps.users.api.views.user.SmeIntegracaoService.atribuir_perfil_coresso'
+    with patch(api_usuario_core_sso_or_none) as mock_usuario_core_sso_or_none:
+        mock_usuario_core_sso_or_none.return_value = None
+        with patch(api_cria_usuario_core_sso) as mock_cria_usuario_core_sso:
+            with patch(api_atribuir_perfil_core_sso) as mock_atribuir_perfil_core_sso:
+                response = jwt_authenticated_client_u.post("/api/usuarios/", data=json.dumps(payload), content_type='application/json')
+
+    result = response.json()
+
+    esperado = {
+        'username': '9876543',
+        'email': 'lukaku@gmail.com',
+        'name': 'Lukaku Silva',
+        'e_servidor': True,
+        'groups': [grupo_1.id, ],
+        'visoes': [visao_ue.id, ],
+
+    }
+    User = get_user_model()
+    u = User.objects.filter(username='9876543').first()
+
+    assert list(u.visoes.values_list('nome', flat=True)) == ["UE", ]
+    assert list(u.unidades.values_list('codigo_eol', flat=True)) == []
+    assert response.status_code == status.HTTP_201_CREATED
+    assert result == esperado
+    mock_usuario_core_sso_or_none.assert_called_once_with(login='9876543')
+    mock_cria_usuario_core_sso.assert_called_once_with(
+        e_servidor=True,
+        email='lukaku@gmail.com',
+        login='9876543',
+        nome='Lukaku Silva'
+    )
+    mock_atribuir_perfil_core_sso.assert_called_once_with(login='9876543', visao='UE')
+
