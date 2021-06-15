4a52e3ecae620d0925056dd642bf6b4b907fa7ba
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-06-15 11:38:18 -0300

    feat(38438): Cria parâmetro de notificação proximidade PC
    Cria o parametro para definir quantos dias antes do inicio
    do período de prestações de contas os usuários serão
    notificados.

--- sme_ptrf_apps/core/models/parametros.py
+++ sme_ptrf_apps/core/models/parametros.py
@@ -6,10 +6,19 @@ from sme_ptrf_apps.core.models_abstracts import ModeloBase, SingletonModel
 
 class Parametros(SingletonModel, ModeloBase):
     permite_saldo_conta_negativo = models.BooleanField('Permite saldo negativo em contas?', default=True)
+
     fique_de_olho = RichTextField(null=True)
     fique_de_olho_relatorio_dre = RichTextField(null=True, verbose_name='Fique de olho (Relatório DRE)')
+
     tempo_notificar_nao_demonstrados = models.PositiveSmallIntegerField(
-        'Tempo para notificação de transações não demonstradas (dias)', default=0)
+        'Tempo para notificação de transações não demonstradas (dias)',
+        default=0
+    )
+
+    dias_antes_inicio_periodo_pc_para_notificacao = models.PositiveSmallIntegerField(
+        'Quantos dias antes do inicio do período de PC, os usuários serão notificados?',
+        default=5
+    )
 
     def __str__(self):
         return 'Parâmetros do PTRF'
