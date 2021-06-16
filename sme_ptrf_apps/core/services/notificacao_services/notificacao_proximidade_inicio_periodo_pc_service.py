0d2b0a8f42ab6781e3999c3fde3131d16da60fe8
Alessandro Fernandes <alcfernandes@yahoo.com>
2021-06-16 15:25:53 -0300

    feat(38438): Notificação proximidade inicio pc
    Cria o serviço que gerará as notificações de
    proximidade de inicio de prestação de contas.

--- sme_ptrf_apps/core/services/notificacao_services/notificacao_proximidade_inicio_periodo_pc_service.py
+++ sme_ptrf_apps/core/services/notificacao_services/notificacao_proximidade_inicio_periodo_pc_service.py
@@ -17,12 +17,12 @@ def notificar_proximidade_inicio_periodo_prestacao_conta():
     data_alvo = date.today() + timedelta(days=dias_antes)
 
     periodo = Periodo.objects.filter(
-        data_inicio_prestacao_contas__lte=data_de_hoje,
-        data_fim_prestacao_contas__gte=data_de_hoje,
-        notificacao_inicio_periodo_pc_realizada=False
+        data_inicio_prestacao_contas__lte=data_alvo,
+        data_fim_prestacao_contas__gte=data_alvo,
+        notificacao_proximidade_inicio_pc_realizada=False
     ).first()
 
-    users = get_users_by_permission('recebe_notificacao_inicio_periodo_prestacao_de_contas')
+    users = get_users_by_permission('recebe_notificacao_proximidade_inicio_prestacao_de_contas')
 
     if users:
         try:
@@ -36,16 +36,20 @@ def notificar_proximidade_inicio_periodo_prestacao_conta():
             for user in users:
                 logger.info(f"Gerando notificação para o usuario: {user} | Período: {periodo.referencia}")
 
+                dias_para_inicio = periodo.data_inicio_prestacao_contas - date.today()
+
                 Notificacao.notificar(
                     tipo=Notificacao.TIPO_NOTIFICACAO_INFORMACAO,
                     categoria=Notificacao.CATEGORIA_NOTIFICACAO_ELABORACAO_PC,
                     remetente=Notificacao.REMETENTE_NOTIFICACAO_SISTEMA,
-                    titulo=f"O período de envio da PC de {periodo.referencia} começou",
-                    descricao="O período de prestações de contas já foi iniciado. Fique atento para não perder o prazo e envie os documentos da prestação de contas",
+                    titulo=f"O período de envio da PC de {periodo.referencia} está se aproximando.",
+                    descricao=f"Faltam apenas {dias_para_inicio.days} dias para o início do período de prestações "
+                              f"de contas. Finalize o cadastro de crédito e de gastos, a conciliação bancária e "
+                              f"gere os documentos da prestação de contas.",
                     usuario=user
                 )
-                periodo.notificacao_inicio_prestacao_de_contas_realizada()
+                periodo.notificacao_proximidade_inicio_prestacao_de_contas_realizada()
         else:
-            logger.info(f"Não foram encontrados usuários a serem notificados sobre início do período de prestação de contas")
+            logger.info(f"Não foram encontrados usuários a serem notificados.")
     else:
-        logger.info(f"Não foram encontrados períodos a serem notificados sobre início do período de prestação de contas")
+        logger.info(f"Não foram encontrados períodos a serem notificados.")
---
+++ sme_ptrf_apps/core/tests/tests_notificacoes_services/test_notificacoes_proximidade_periodo_pc_service.py
@@ -0,0 +1,91 @@
+import pytest
+
+from freezegun import freeze_time
+
+from ....core.models import Notificacao, Periodo
+from ....core.services.notificacao_services import notificar_proximidade_inicio_periodo_prestacao_conta
+
+pytestmark = pytest.mark.django_db
+
+
+@freeze_time("2021-03-27")
+def test_deve_notificar_usuarios_5_dias_antes(
+    associacao_a,
+    usuario_notificavel,
+    usuario_nao_notificavel,
+    periodo_2020_4_pc_2021_01_01_a_2021_01_15,
+    periodo_2021_1_pc_2021_04_1_a_2021_04_15,
+    periodo_2021_2_pc_2021_07_01_a_2021_07_15,
+    parametro_proximidade_inicio_pc_5_dias,
+):
+
+    assert not Notificacao.objects.exists()
+
+    notificar_proximidade_inicio_periodo_prestacao_conta()
+
+    assert Notificacao.objects.count() == 1
+
+    notificacao = Notificacao.objects.first()
+
+    assert notificacao.tipo == Notificacao.TIPO_NOTIFICACAO_INFORMACAO
+    assert notificacao.categoria == Notificacao.CATEGORIA_NOTIFICACAO_ELABORACAO_PC
+    assert notificacao.remetente == Notificacao.REMETENTE_NOTIFICACAO_SISTEMA
+    assert notificacao.titulo == 'O período de envio da PC de 2021.1 está se aproximando.'
+    assert notificacao.descricao == 'Faltam apenas 5 dias para o início do período de prestações de contas. Finalize o cadastro de crédito e de gastos, a conciliação bancária e gere os documentos da prestação de contas.'
+    assert notificacao.usuario == usuario_notificavel
+
+
+@freeze_time("2021-04-01")
+def test_deve_notificar_usuarios_no_dia(
+    associacao_a,
+    usuario_notificavel,
+    usuario_nao_notificavel,
+    periodo_2020_4_pc_2021_01_01_a_2021_01_15,
+    periodo_2021_1_pc_2021_04_1_a_2021_04_15,
+    periodo_2021_2_pc_2021_07_01_a_2021_07_15,
+    parametro_proximidade_inicio_pc_5_dias,
+):
+
+    assert not Notificacao.objects.exists()
+
+    notificar_proximidade_inicio_periodo_prestacao_conta()
+
+    assert Notificacao.objects.count() == 1
+
+
+@freeze_time("2021-04-01")
+def test_deve_flagar_periodo_como_notificado(
+    associacao_a,
+    usuario_notificavel,
+    usuario_nao_notificavel,
+    periodo_2020_4_pc_2021_01_01_a_2021_01_15,
+    periodo_2021_1_pc_2021_04_1_a_2021_04_15,
+    periodo_2021_2_pc_2021_07_01_a_2021_07_15,
+    parametro_proximidade_inicio_pc_5_dias,
+):
+
+    assert not periodo_2021_1_pc_2021_04_1_a_2021_04_15.notificacao_proximidade_inicio_pc_realizada
+
+    notificar_proximidade_inicio_periodo_prestacao_conta()
+
+    periodo = Periodo.objects.get(referencia="2021.1")
+    assert periodo.notificacao_proximidade_inicio_pc_realizada
+
+
+@freeze_time("2021-04-01")
+def test_nao_deve_re_notificar_periodo(
+    associacao_a,
+    usuario_notificavel,
+    usuario_nao_notificavel,
+    periodo_2020_4_pc_2021_01_01_a_2021_01_15,
+    periodo_2021_1_pc_2021_04_1_a_2021_04_15,
+    periodo_2021_2_pc_2021_07_01_a_2021_07_15,
+    parametro_proximidade_inicio_pc_5_dias,
+):
+    periodo_2021_1_pc_2021_04_1_a_2021_04_15.notificacao_proximidade_inicio_prestacao_de_contas_realizada()
+
+    notificar_proximidade_inicio_periodo_prestacao_conta()
+
+    assert not Notificacao.objects.exists()
+
+
