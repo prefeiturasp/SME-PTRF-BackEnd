31a5a90f4961d6b401980ac01bccda4588fca448
ollyvergithub <ollyverottoboni@gmail.com>
2021-06-17 17:01:57 -0300

    Finalizando Feature 38442 - [Associação] Gerar notificações sobre ajustes solicitados em prestação de contas

---
+++ sme_ptrf_apps/core/services/notificacao_services/formata_data_dd_mm_yyyy.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+def formata_data_dd_mm_yyyy(data):
+    data_formatada = " - "
+    if data:
+        d = datetime.strptime(str(data), '%Y-%m-%d')
+        data_formatada = d.strftime("%d/%m/%Y")
+
+    return f'{data_formatada}'
---
+++ sme_ptrf_apps/core/tests/tests_notificacoes_services/test_notificacoes_pc_devolvida_para_acertos.py
@@ -0,0 +1,32 @@
+import pytest
+
+import datetime
+from ....core.services.notificacao_services.formata_data_dd_mm_yyyy import formata_data_dd_mm_yyyy
+
+from ....core.models import Notificacao
+from ....core.services.notificacao_services import notificar_prestacao_de_contas_devolvida_para_acertos
+
+pytestmark = pytest.mark.django_db
+
+
+def test_deve_notificar_usuarios(prestacao_notifica_prestacao_de_contas_devolvida_para_acertos, usuario_notificavel, associacao_a):
+    assert not Notificacao.objects.exists()
+    data_limite_ue = datetime.date(2020, 6, 17)
+    notificar_prestacao_de_contas_devolvida_para_acertos(prestacao_notifica_prestacao_de_contas_devolvida_para_acertos, data_limite_ue)
+    assert Notificacao.objects.count() == 1
+    notificacao = Notificacao.objects.first()
+
+    assert notificacao.tipo == Notificacao.TIPO_NOTIFICACAO_ALERTA
+    assert notificacao.categoria == Notificacao.CATEGORIA_NOTIFICACAO_ANALISE_PC
+    assert notificacao.remetente == Notificacao.REMETENTE_NOTIFICACAO_DRE
+    assert notificacao.titulo == f"Ajustes necessários na PC"
+    assert notificacao.descricao == f"A DRE solicitou alguns ajustes em sua prestação de contas do período {prestacao_notifica_prestacao_de_contas_devolvida_para_acertos.periodo.referencia}. O seu prazo para envio das mudanças é {formata_data_dd_mm_yyyy(data_limite_ue)}"
+    assert notificacao.usuario == usuario_notificavel
+
+
+def test_nao_deve_notificar_usuarios_sem_permissao(prestacao_notifica_prestacao_de_contas_devolvida_para_acertos, usuario_nao_notificavel, associacao_a):
+    assert not Notificacao.objects.exists()
+    data_limite_ue = datetime.date(2020, 6, 17)
+    notificar_prestacao_de_contas_devolvida_para_acertos(prestacao_notifica_prestacao_de_contas_devolvida_para_acertos, data_limite_ue)
+    assert Notificacao.objects.count() == 0
+
