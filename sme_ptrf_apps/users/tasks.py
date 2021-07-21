bfef7febcea0d24415bc36bc1c0561c80cd8a00b
ollyvergithub <ollyverottoboni@gmail.com>
2021-07-21 10:49:43 -0300

    feat(34048): Cria método, tarefa asíncrona e template para envio de email nova notificação

--- sme_ptrf_apps/users/tasks.py
+++ sme_ptrf_apps/users/tasks.py
@@ -6,7 +6,7 @@ import environ
 from celery import shared_task
 from config import celery_app
 
-from sme_ptrf_apps.core.services.enviar_email import enviar_email_html
+from sme_ptrf_apps.core.services.enviar_email import enviar_email_html, enviar_email_nova_notificacao_html
 
 env = environ.Env()
 logger = logging.getLogger(__name__)
@@ -29,6 +29,31 @@ def enviar_email_redifinicao_senha(email, username, nome, hash_definicao):
     }
     return enviar_email_html(
         assunto='Solicitação de redefinição de senha',
+        template='email_redefinicao_senha.html',
+        context=context,
+        enviar_para=email
+    )
+
+
+@shared_task(
+    autoretry_for=(SMTPServerDisconnected,),
+    retry_backoff=2,
+    retry_kwargs={'max_retries': 8},
+)
+def enviar_email_nova_notificacao_async(email, username, nome, titulo, descricao):
+    logger.info("Tarefa de envio de email nova notificacao")
+    link = f"https://{env('SERVER_NAME')}/login/"
+    data = date.today().strftime("%d/%m/%Y")
+    context = {
+        'url': link,
+        'nome': nome,
+        'login': username,
+        'descricao': descricao,
+        'server_name': f"https://{env('SERVER_NAME')}",
+        'data': data
+    }
+    return enviar_email_nova_notificacao_html(
+        assunto=titulo,
         template='email_redefinicao_senha.html',
         context=context,
         enviar_para=email
