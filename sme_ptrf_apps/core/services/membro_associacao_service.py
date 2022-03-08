856dadf27541e3d9fd114da411c98a200ae77feb
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-03-08 15:13:34 -0300

    fix(60095): Corrige busca de cargo de servidor
    Altera a lógica da pesquisa de cargo do servidor para retornar
    preferencialmente os cargos de diretor, coordenador ou assistente, nessa
    ordem de prioridade, caso existam, ou o primeiro registro, caso não
    exista nenhum deles.
    
    Closes AB#60095

--- sme_ptrf_apps/core/services/membro_associacao_service.py
+++ sme_ptrf_apps/core/services/membro_associacao_service.py
@@ -65,8 +65,35 @@ class TerceirizadasService:
             results = response.json()['results']
             logger.info("Dados do servidor: %r", results)
             if len(results) >= 1:
-                return results
+                return seleciona_cargo_servidor(results)
             raise TerceirizadasException(f'Não foram encontrados resultados para o RF: {registro_funcional}.')
         else:
             raise TerceirizadasException(f'API EOL com erro. Status: {response.status_code}')
+
+
+def seleciona_cargo_servidor(result_info_servidor):
+    """
+    Escolhe na lista de funções de um servidor apenas uma conforme regras de priorização de cargo
+    """
+
+    # Se hpuve apenas um registro, retorna o próprio
+    if len(result_info_servidor) <= 1:
+        return result_info_servidor
+
+    # Procura pelo cargo de diretor
+    result = next((info for info in result_info_servidor if "DIRETOR" in info["cargo"]), None)
+
+    if not result:
+        # Procura pelo cargo de coordenador
+        result = next((info for info in result_info_servidor if "COORDENADOR" in info["cargo"]), None)
+
+    if not result:
+        # Procura pelo cargo de assistente
+        result = next((info for info in result_info_servidor if "ASSISTENTE" in info["cargo"]), None)
+
+    if not result:
+        # Retorna o primeiro
+        result = result_info_servidor[0]
+
+    return [result]
 
