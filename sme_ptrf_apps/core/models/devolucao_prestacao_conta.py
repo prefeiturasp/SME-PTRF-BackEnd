ff34d520bce40010722f216d17d9837774bd24c5
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-06-03 07:59:57 -0300

    feat(64261): Config AuditLog para DevolucaoPrestacaoConta

--- sme_ptrf_apps/core/models/devolucao_prestacao_conta.py
+++ sme_ptrf_apps/core/models/devolucao_prestacao_conta.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+from auditlog.models import AuditlogHistoryField
+from auditlog.registry import auditlog
+
 from sme_ptrf_apps.core.models_abstracts import ModeloBase
 
 
 class DevolucaoPrestacaoConta(ModeloBase):
+    history = AuditlogHistoryField()
+
     prestacao_conta = models.ForeignKey('PrestacaoConta', on_delete=models.CASCADE,
                                         related_name='devolucoes_da_prestacao')
 
@@ -19,3 +24,6 @@ class DevolucaoPrestacaoConta(ModeloBase):
     class Meta:
         verbose_name = "Devolução de prestação de contas"
         verbose_name_plural = "09.7) Devoluções de prestações de contas"
+
+
+auditlog.register(DevolucaoPrestacaoConta)
