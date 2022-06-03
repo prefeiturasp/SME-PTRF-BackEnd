2c2482acf3cd339e5a71f6e6f5abd0d3a7783cba
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-06-03 07:53:39 -0300

    feat(64261): Config AuditLog para SolicitacaoAcertoDocumento

--- sme_ptrf_apps/core/tests/tests_solicitacao_acerto_documento/test_solicitacao_acerto_documento_model.py
+++ sme_ptrf_apps/core/tests/tests_solicitacao_acerto_documento/test_solicitacao_acerto_documento_model.py
@@ -25,3 +25,13 @@ def test_srt_model(solicitacao_acerto_documento_ata):
 def test_admin():
     # pylint: disable=W0212
     assert admin.site._registry[SolicitacaoAcertoDocumento]
+
+
+def test_audit_log(solicitacao_acerto_documento_ata):
+    assert solicitacao_acerto_documento_ata.history.count() == 1  # Um log de inclusão
+    assert solicitacao_acerto_documento_ata.history.latest().action == 0  # 0-Inclusão
+
+    solicitacao_acerto_documento_ata.detalhamento = "TESTE"
+    solicitacao_acerto_documento_ata.save()
+    assert solicitacao_acerto_documento_ata.history.count() == 2  # Um log de inclusão e outro de edição
+    assert solicitacao_acerto_documento_ata.history.latest().action == 1  # 1-Edição
