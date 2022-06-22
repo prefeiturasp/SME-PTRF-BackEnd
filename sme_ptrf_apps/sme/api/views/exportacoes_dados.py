f28430337529d8ac36962e5e36172d3ceb033467
linikerj <linikerenem@gmail.com>
2022-06-22 16:17:35 -0300

    feat(65001) Cria serviço de exportação de dados credito na branch de develop.

---
+++ sme_ptrf_apps/sme/api/views/exportacoes_dados.py
@@ -0,0 +1,36 @@
+import logging
+
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
+from sme_ptrf_apps.core.models.arquivos_download import ArquivoDownload
+from sme_ptrf_apps.sme.tasks import exportar_receitas_async
+from sme_ptrf_apps.users.permissoes import PermissaoAPIApenasSmeComLeituraOuGravacao
+
+
+logger = logging.getLogger(__name__)
+
+
+class ExportacoesDadosViewSet(GenericViewSet):
+    permission_classes = [IsAuthenticated & PermissaoAPIApenasSmeComLeituraOuGravacao]
+    lookup_field = 'uuid'
+    queryset = ArquivoDownload.objects.all()
+
+    @action(
+        detail=False, methods=['get'],
+        url_path='creditos',
+        permission_classes=permission_classes
+        )
+    def creditos(self, request):
+        exportar_receitas_async(
+            data_inicio=request.query_params.get('data_inicio'),
+            data_final=request.query_params.get('data_final'),
+            username=request.user.username
+        )
+
+        return Response(
+            {'response': "Arquivo gerado com sucesso, enviado para a central de download"},
+            status=HTTP_201_CREATED
+        )
---
+++ sme_ptrf_apps/utils/built_in_custom.py
@@ -0,0 +1,22 @@
+from pyexpat import model
+from pytest import Instance
+
+
+def get_recursive_attr(instance: model, fields: str) -> Instance:
+    """
+    Esse serviço existe para realizar o getattr do python recursivamente.
+    Exemplo usando getattr -> getattr(instance, items, default),
+    no getattr se pode passar apenas um campo, já no get_recursive_attr se pode
+    passar multiplos campos.
+
+    Exemplo: get_recursive_attr(instance, items__subitems, default).
+    """
+    if fields and isinstance(fields, str):
+        fields = fields.split('__')
+
+    field = getattr(instance, fields.pop(0))
+
+    if fields and field:
+        return get_recursive_attr(field, fields)
+
+    return field
