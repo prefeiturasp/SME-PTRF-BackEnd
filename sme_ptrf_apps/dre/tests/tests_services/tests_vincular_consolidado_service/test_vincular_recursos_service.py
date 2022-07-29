ce6396aa177bddfb85dd5b23ef5d561e8490e084
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-07-29 11:30:25 -0300

    feat(70112): Cria ação integrar consolidado ata parecer técnico
    Altera o admin de atas de parecer técnico para incluir uma ação
    para integrar as atas a um consolidado DRE.

--- sme_ptrf_apps/dre/tests/tests_services/tests_vincular_consolidado_service/test_vincular_recursos_service.py
+++ sme_ptrf_apps/dre/tests/tests_services/tests_vincular_consolidado_service/test_vincular_recursos_service.py
@@ -1,6 +1,6 @@
 import pytest
 
-from sme_ptrf_apps.dre.models import RelatorioConsolidadoDRE
+from sme_ptrf_apps.dre.models import RelatorioConsolidadoDRE, AtaParecerTecnico
 from sme_ptrf_apps.dre.services.vincular_consolidado_service import VincularConsolidadoService
 
 pytestmark = pytest.mark.django_db
@@ -37,3 +37,45 @@ def test_vincular_consolidado_service_vincular_artefato_criando_consolidado_inex
     assert relatorio.consolidado_dre.sequencia_de_publicacao == 0
     assert relatorio.consolidado_dre.eh_parcial is False
     assert relatorio.consolidado_dre.status == ConsolidadoDRE.STATUS_GERADOS_TOTAIS
+
+
+def test_vincular_consolidado_service_vincular_artefato_usando_ultima_sequencia_de_consolidados_existentes(
+    vcs_periodo_2022_1,
+    vcs_dre_ipiranga,
+    vcs_consolidado_dre_ipiranga_2022_1_parcial_1,
+    vcs_consolidado_dre_ipiranga_2022_1_parcial_2,
+    vcs_relatorio_dre_consolidado_gerado_total_desvinculado
+):
+    VincularConsolidadoService.vincular_artefato(vcs_relatorio_dre_consolidado_gerado_total_desvinculado)
+
+    relatorio = RelatorioConsolidadoDRE.by_id(vcs_relatorio_dre_consolidado_gerado_total_desvinculado.id)
+
+    assert relatorio.consolidado_dre == vcs_consolidado_dre_ipiranga_2022_1_parcial_2
+
+
+def test_vincular_consolidado_service_vincular_ata_gravando_ultima_sequencia_de_consolidados_existentes(
+    vcs_periodo_2022_1,
+    vcs_dre_ipiranga,
+    vcs_consolidado_dre_ipiranga_2022_1_parcial_1,
+    vcs_consolidado_dre_ipiranga_2022_1_parcial_2,
+    vcs_ata_parecer_tecnico_desvinculada
+):
+    VincularConsolidadoService.vincular_artefato(vcs_ata_parecer_tecnico_desvinculada)
+
+    ata = AtaParecerTecnico.by_id(vcs_ata_parecer_tecnico_desvinculada.id)
+
+    assert ata.consolidado_dre == vcs_consolidado_dre_ipiranga_2022_1_parcial_2
+    assert ata.sequencia_de_publicacao == 2
+
+
+def test_vincular_consolidado_service_vincular_ata_gravando_sequencia_de_consolidado_criado(
+    vcs_periodo_2022_1,
+    vcs_dre_ipiranga,
+    vcs_ata_parecer_tecnico_desvinculada
+):
+    VincularConsolidadoService.vincular_artefato(vcs_ata_parecer_tecnico_desvinculada)
+
+    ata = AtaParecerTecnico.by_id(vcs_ata_parecer_tecnico_desvinculada.id)
+
+    assert ata.consolidado_dre is not None
+    assert ata.sequencia_de_publicacao == 0
