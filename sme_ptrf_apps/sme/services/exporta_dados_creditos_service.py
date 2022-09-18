46d1f48ecace3d27802c7f6e4b2d0e34fe1cfd85
linikerj <linikerenem@gmail.com>
2022-09-18 03:00:10 -0300

    feat(73295) Adiciona campos em receita principal.csv

--- sme_ptrf_apps/sme/services/exporta_dados_creditos_service.py
+++ sme_ptrf_apps/sme/services/exporta_dados_creditos_service.py
@@ -43,6 +43,8 @@ CABECALHO_RECEITA = [
         ('Outros motivos para estorno (no caso de estorno)', 'outros_motivos_estorno'),
         ('Data e hora de criação do registro', 'criado_em'),
         ('Data e hora da última atualização do registro', 'alterado_em'),
+        ('Status do crédito', 'status'),
+        ('Data e hora de inativação', 'data_e_hora_de_inativacao'),
         ('UUID do crédito', 'uuid'),
     ],
 CABECALHO_MOTIVOS_ESTORNO = [
