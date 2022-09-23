ed4ba30227ecb630bd667dd1a38d01dd3778901c
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-09-23 17:18:48 -0300

    fix(74103): Altera sql de verificação de devoluções ausentes

--- sme_ptrf_apps/contrib/scripts_diversos/dev_tesouro.sql
+++ sme_ptrf_apps/contrib/scripts_diversos/dev_tesouro.sql
@@ -3,15 +3,15 @@
 -- Lista solicitações de devolução ao tesouro sem uma devolução ao tesouro vinculada
 select
 a.unidade_id as "codigo_eol",
-a.nome,
+u.nome,
 pc.id as "pc_id",
 p.referencia,
-d.id as "despesa_id", d.numero_documento,
-count(sal.id)
+d.id as "despesa_id", d.numero_documento
 from
 core_prestacaoconta as pc,
 core_periodo as p,
 core_associacao as a,
+core_unidade as u,
 core_analiseprestacaoconta as apc,
 core_analiselancamentoprestacaoconta as al,
 despesas_despesa as d,
@@ -19,6 +19,7 @@ core_solicitacaoacertolancamento as sal,
 core_tipoacertolancamento as tal
 where
 pc.periodo_id = p.id
+and a.unidade_id = u.codigo_eol
 and pc.associacao_id = a.id
 and apc.prestacao_conta_id = pc.id
 and al.analise_prestacao_conta_id = apc.id
@@ -31,13 +32,13 @@ group by
 pc.id,
 p.referencia,
 a.unidade_id,
-a.nome,
+u.nome,
 d.id, d.numero_documento, d.valor_total
 order by
 a.unidade_id,
-a.nome,
+u.nome,
 p.referencia,
-d.id, d.numero_documento, d.valor_total
+d.id, d.numero_documento
 
 
 -- Lista de Prestações de Conta com devoluções ao tesouro faltando
