6e5b357cd1bcd8b5a90530359fb6f3ec0a643243
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-09-27 07:52:48 -0300

    fix(74103): Altera carga dev-tesouro para datas vazias

--- sme_ptrf_apps/contrib/scripts_diversos/dev_tesouro.sql
+++ sme_ptrf_apps/contrib/scripts_diversos/dev_tesouro.sql
@@ -6,7 +6,7 @@ a.unidade_id as "codigo_eol",
 u.nome,
 pc.id as "pc_id",
 p.referencia,
-d.id as "despesa_id", d.numero_documento
+d.id as "despesa_id", d.numero_documento, d.valor_total
 from
 core_prestacaoconta as pc,
 core_periodo as p,
@@ -27,7 +27,7 @@ and al.despesa_id = d.id
 and sal.analise_lancamento_id = al.id
 and tal.id = sal.tipo_acerto_id
 and tal.categoria = 'DEVOLUCAO'
-and sal.devolucao_ao_tesouro_id is null
+and (select count(dt.id) from core_devolucaoaotesouro as dt where dt.prestacao_conta_id = pc.id and dt.despesa_id = d.id) = 0
 group by
 pc.id,
 p.referencia,
@@ -40,17 +40,17 @@ u.nome,
 p.referencia,
 d.id, d.numero_documento
 
-
 -- Lista de Prestações de Conta com devoluções ao tesouro faltando
 select
 a.unidade_id as "codigo_eol",
-a.nome,
+u.nome,
 pc.id as "pc_id",
 p.referencia
 from
 core_prestacaoconta as pc,
 core_periodo as p,
 core_associacao as a,
+core_unidade as u,
 core_analiseprestacaoconta as apc,
 core_analiselancamentoprestacaoconta as al,
 despesas_despesa as d,
@@ -58,6 +58,7 @@ core_solicitacaoacertolancamento as sal,
 core_tipoacertolancamento as tal
 where
 pc.periodo_id = p.id
+and a.unidade_id = u.codigo_eol
 and pc.associacao_id = a.id
 and apc.prestacao_conta_id = pc.id
 and al.analise_prestacao_conta_id = apc.id
@@ -65,13 +66,13 @@ and al.despesa_id = d.id
 and sal.analise_lancamento_id = al.id
 and tal.id = sal.tipo_acerto_id
 and tal.categoria = 'DEVOLUCAO'
-and sal.devolucao_ao_tesouro_id is null
+and (select count(dt.id) from core_devolucaoaotesouro as dt where dt.prestacao_conta_id = pc.id and dt.despesa_id = d.id) = 0
 group by
 pc.id,
 p.referencia,
 a.unidade_id,
-a.nome
+u.nome
 order by
 a.unidade_id,
-a.nome,
+u.nome,
 p.referencia
