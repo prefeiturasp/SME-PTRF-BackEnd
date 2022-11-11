1ad332d73d73e6ba765ffaff4c277fbb7a6a4ee8
linikerj <linikerenem@gmail.com>
2022-11-11 14:02:40 -0300

    ajusta teste que estava quebrando no back

--- sme_ptrf_apps/dre/api/serializers/consolidado_dre_serializer.py
+++ sme_ptrf_apps/dre/api/serializers/consolidado_dre_serializer.py
@@ -33,6 +33,7 @@ class ConsolidadoDreComDocumentosSerializer(serializers.ModelSerializer):
             'laudas_do_consolidado_dre',
         )
 
+
 class ConsolidadoDreDetalhamentoSerializer(serializers.ModelSerializer):
     tipo_relatorio = serializers.SerializerMethodField('get_tipo_relatorio')
     exibe_reabrir_relatorio = serializers.SerializerMethodField('get_exibe_reabrir_relatorio')
