cce7a308c344c22498173d826452e9005423533d
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-11-22 09:25:52 -0300

    fix(79496): Inclui tratamento de erro em prévia de relatório de acertos
    Agora caso ocorra alguma exceção na geração de uma prévia de relatório
    de acertos, o status volta para "NÃO GERADO" permitindo ao usuário uma
    nova geração da prévia.

--- sme_ptrf_apps/core/models/analise_prestacao_conta.py
+++ sme_ptrf_apps/core/models/analise_prestacao_conta.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db import models
 
 from auditlog.models import AuditlogHistoryField
@@ -5,6 +7,8 @@ from auditlog.registry import auditlog
 
 from sme_ptrf_apps.core.models_abstracts import ModeloBase
 from datetime import datetime
+
+logger = logging.getLogger(__name__)
 
 
 class AnalisePrestacaoConta(ModeloBase):
@@ -150,6 +154,15 @@ class AnalisePrestacaoConta(ModeloBase):
         self.status_versao = self.STATUS_CONCLUIDO
         self.arquivo_pdf_criado_em = datetime.today()
         self.save()
+
+    def cancela_geracao_arquivo_pdf(self):
+        logging.info(f'Cancelando geração de arquivo pdf da análise {self.pk}')
+        self.arquivo_pdf = None
+        self.versao = self.VERSAO_NAO_GERADO
+        self.status_versao = self.STATUS_NAO_GERADO
+        self.arquivo_pdf_criado_em = None
+        self.save()
+        logging.info(f'Geração de arquivo pdf da análise {self.pk} cancelada')
 
     class Meta:
         verbose_name = "Análise de prestação de contas"
