a42c33a2e77f7772972ec7a63389979b9d7a363a
Liniker Oliveira <linikerenem@gmail.com>
2022-11-24 08:46:13 -0300

    Feat(79387) incluir notificacao sobre prazo (#1615)
    * feat(79387) Cria serviço e task assincrona para verificar dre fora de notificaçoes
    
    * feat(79387) Remove ipdb para debug.
    
    * feat(79387) Refatora funções para OOP e altera estrutura de pasta para DRE
    
    * feat(79387) Adiciona um teste a mais e refatora outras classe para OOP
    
    * feat(79387) Ajusta POO em task que chama a devolução
    
    * feat(79387) Ajuste em teste e core para prazo.

---
+++ sme_ptrf_apps/dre/tests/tests_services/tests_notificacoes_services/test_notificacoes_prazo_para_acerto_apos_vencimento_service.py
@@ -0,0 +1,43 @@
+import pytest
+
+from freezegun import freeze_time
+
+from .....core.models import Notificacao
+from .....dre.services.notificacao_service.notificacao_prazo_para_acerto_apos_vencimento import NotificacaoConsolidadoPrazoAcertoVencimento
+
+
+pytestmark = pytest.mark.django_db
+
+
+@freeze_time("2022-11-22")
+def test_deve_notificar_tecnico_dre(usuario_tecnico_notificavel, associacao_a, consolidado_dre_devolucao_apos_acertos):
+    assert not Notificacao.objects.exists()
+    NotificacaoConsolidadoPrazoAcertoVencimento(
+        enviar_email=True
+    ).notificar_prazo_para_acerto_apos_vencimento()
+    notificacao = Notificacao.objects.first()
+    assert Notificacao.objects.count() == 1
+    assert notificacao.tipo == Notificacao.TIPO_NOTIFICACAO_ALERTA
+    assert notificacao.categoria == Notificacao.CATEGORIA_NOTIFICACAO_DEVOLUCAO_CONSOLIDADO_APOS_PRAZO_VENCIMENTO
+    assert notificacao.remetente == Notificacao.REMETENTE_NOTIFICACAO_SISTEMA
+    assert notificacao.titulo == f'Devolução para acertos no relatório consolidado de {consolidado_dre_devolucao_apos_acertos.periodo.referencia}'
+    assert notificacao.descricao == f"O prazo para acerto da Publicação {consolidado_dre_devolucao_apos_acertos.referencia} {consolidado_dre_devolucao_apos_acertos.periodo.referencia} expirou. Favor verificar os acertos solicitados e regularizar a situação."
+    assert notificacao.usuario == usuario_tecnico_notificavel
+
+
+@freeze_time("2022-11-22")
+def test_nao_deve_notificar_pois_nao_tem_acerto_fora_prazo(periodo_2021_4_pc_2021_06_18_a_2021_06_30, usuario_notificavel, associacao_a, consolidado_dre_devolucao_apos_acertos_dentro_do_prazo):
+    assert not Notificacao.objects.exists()
+    NotificacaoConsolidadoPrazoAcertoVencimento(
+        enviar_email=True
+    ).notificar_prazo_para_acerto_apos_vencimento()
+    assert Notificacao.objects.count() == 0
+
+
+@freeze_time("2022-11-22")
+def test_nao_deve_notificar_pois_nao_tem_usuario(periodo_2021_4_pc_2021_06_18_a_2021_06_30, usuario_notificavel, associacao_a, consolidado_dre_devolucao_apos_acertos_dentro_do_prazo):
+    assert not Notificacao.objects.exists()
+    NotificacaoConsolidadoPrazoAcertoVencimento(
+        enviar_email=True
+    ).notificar_prazo_para_acerto_apos_vencimento()
+    assert Notificacao.objects.count() == 0
