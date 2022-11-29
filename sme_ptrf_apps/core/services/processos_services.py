b38fdb0bcfb18e358b84f9901a292fe17d860ca4
Alessandro Cunha Fernandes <alessandro.c.fernandes@gmail.com>
2022-11-29 09:03:44 -0300

    Hotfix/1.33.12 update develop (#1640)
    * Hotfix/1.33.10 (#1612)
    
    * fix(79038): Corrige conclusão consolidado dre
    
    A conclusão antes não apagava prévias parciais de um consolidado único
    ou prévias únicas de um consolidado parcial.
    
    * docs(1.33.10): Atualiza release notes e versão
    
    * Update ci-cd.yml
    
    * Hotfix/1.33.11 (#1618)
    
    * fix(79496): Inclui tratamento de erro em prévia de relatório de acertos
    
    Agora caso ocorra alguma exceção na geração de uma prévia de relatório
    de acertos, o status volta para "NÃO GERADO" permitindo ao usuário uma
    nova geração da prévia.
    
    * fix(79496): Configura integração Celery e Sentry
    
    Agora erros ocorridos em processos Celery também alimentarão
    o Sentry.
    
    * fix(79315): Corrige layout consolidado DRE
    
    Corrige deslocamento na tabela do bloco 2 quando há uma justificativa de
    diferença de repasses informada.
    
    O problema era causado por uma divergência no colSpan.
    
    * docs(1.33.11): Atualiza versão e hotfixes.md
    
    * fix(79686): Corrige layout consolidado dre
    
    Corrige má distribuição das linhas, na coluna das despesas, quando não
    há o recurso de livre aplicação
    
    * fix(79837): Corrige lista de pcs para acompanhamento de PC
    
    Informações de técnico, data de recebimento, data de última análise e
    devolução ao tesouro não eram incluídas nas consultas de alguns status
    de PC.
    
    * Update ci-cd.yml
    
    ajuste pytest database postgres
    
    * fix(hf.79686): Corrige má distribuição de linhas na execução financeira
    
    Remove labels de teste
    
    * fix(1.3.12): Atualiza versão e hotfixes.md
    
    Co-authored-by: Luiz Henrique Priotto <luizh_priotto@hotmail.com>

--- sme_ptrf_apps/core/services/processos_services.py
+++ sme_ptrf_apps/core/services/processos_services.py
@@ -1,5 +1,11 @@
 from ..models import ProcessoAssociacao
 
+
 def get_processo_sei_da_prestacao(prestacao_contas):
     return ProcessoAssociacao.by_associacao_periodo(associacao=prestacao_contas.associacao,
                                                     periodo=prestacao_contas.periodo)
+
+
+def get_processo_sei_da_associacao_no_periodo(associacao, periodo):
+    return ProcessoAssociacao.by_associacao_periodo(associacao=associacao,
+                                                    periodo=periodo)
