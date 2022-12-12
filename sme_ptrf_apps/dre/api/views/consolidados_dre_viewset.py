dc12ce728337567d867c942ceca79129e9e8baac
Alessandro Fernandes <alcfernandes@yahoo.com>
2022-12-12 09:23:45 -0300

    feat(74315): Cria endpoint 'api/consolidados-dre/[uuid]/retificar'
    Cria o endpoint que executa o serviço de retificação de um consolidado.

--- sme_ptrf_apps/dre/api/views/consolidados_dre_viewset.py
+++ sme_ptrf_apps/dre/api/views/consolidados_dre_viewset.py
@@ -1189,3 +1189,27 @@ class ConsolidadosDreViewSet(mixins.RetrieveModelMixin,
         pcs = consolidado.pcs_retificaveis()
         return Response(PrestacaoContaListRetificaveisSerializer(pcs, many=True).data, status=status.HTTP_200_OK)
 
+    @action(detail=True, methods=['post'], url_path='retificar', permission_classes=[IsAuthenticated & PermissaoAPIApenasDreComGravacao])
+    def retificar(self, request, uuid):
+        from sme_ptrf_apps.dre.api.serializers.validation_serializers.consolidado_dre_retificacao import ConsolidadoDreRetificacaoSerializer
+        from sme_ptrf_apps.dre.services.consolidado_dre_service import retificar_consolidado_dre
+        consolidado: ConsolidadoDRE = self.get_object()
+
+        query = ConsolidadoDreRetificacaoSerializer(data=request.data)
+        query.is_valid(raise_exception=True)
+
+        try:
+            retificar_consolidado_dre(
+                consolidado_dre=consolidado,
+                prestacoes_de_conta_a_retificar=query.validated_data['pcs_a_retificar'],
+                motivo_retificacao=query.validated_data['motivo_retificacao'],
+            )
+        except Exception as e:
+            erro = {
+                'erro': 'Erro ao retificar o consolidado',
+                'mensagem': f"Não foi possível retificar o consolidado: {e}"
+            }
+            logger.info('Erro: %r', erro)
+            return Response(erro, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(ConsolidadoDreSerializer(consolidado, many=False).data, status=status.HTTP_200_OK)
