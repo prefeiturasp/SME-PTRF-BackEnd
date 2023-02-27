be4b4dc1417afbbcf0cf0c398d53ae619db50ca9
Lucas Santos Rocha <67693972+Lucas-Santos-Rocha-dev@users.noreply.github.com>
2023-02-27 11:40:51 -0300

    feat(85105): Retificação da retificação (#1801)
    * feat(85105): Retificação da retificação
    
    Altera retorno de pcs do consolidado para se adequar a cada situação dependendo do tipo do consolidado (original, retificacao, retificacao da retificacao)
    
    * feat(85105): Retificação da retificação
    
    Melhora condição serializer ata

--- sme_ptrf_apps/dre/services/tests/tests_consolidado_dre_service/test_retornar_pcs_do_consolidado.py
+++ sme_ptrf_apps/dre/services/tests/tests_consolidado_dre_service/test_retornar_pcs_do_consolidado.py
@@ -6,16 +6,16 @@ pytestmark = pytest.mark.django_db
 
 def test_retorna_pcs_do_consolidado_com_retificacao(
     jwt_authenticated_client_dre,
-    consolidado_dre_publicado_no_diario_oficial,
+    consolidado_dre_publicado_no_diario_oficial_com_pcs_vinculadas,
     retificacao_dre,
     prestacao_conta_pc1,
     prestacao_conta_pc2,
     prestacao_conta_pc3
 ):
-    result = consolidado_dre_publicado_no_diario_oficial.pcs_vinculadas_ao_consolidado()
+    result = consolidado_dre_publicado_no_diario_oficial_com_pcs_vinculadas.pcs_vinculadas_ao_consolidado()
 
     # Pcs vinculadas ao consolidado original
-    assert len(consolidado_dre_publicado_no_diario_oficial.prestacoes_de_conta_do_consolidado_dre.all()) == 1
+    assert len(consolidado_dre_publicado_no_diario_oficial_com_pcs_vinculadas.prestacoes_de_conta_do_consolidado_dre.all()) == 1
 
     # Pcs vinculadas a retificação
     assert len(retificacao_dre.prestacoes_de_conta_do_consolidado_dre.all()) == 2
@@ -26,14 +26,14 @@ def test_retorna_pcs_do_consolidado_com_retificacao(
 
 def test_retorna_pcs_do_consolidado_sem_retificacao(
     jwt_authenticated_client_dre,
-    consolidado_dre_publicado_no_diario_oficial,
+    consolidado_dre_publicado_no_diario_oficial_com_uma_pc_vinculada,
     retificacao_dre,
     prestacao_conta_pc1,
 ):
-    result = consolidado_dre_publicado_no_diario_oficial.pcs_vinculadas_ao_consolidado()
+    result = consolidado_dre_publicado_no_diario_oficial_com_uma_pc_vinculada.pcs_vinculadas_ao_consolidado()
 
     # Pcs vinculadas ao consolidado original
-    assert len(consolidado_dre_publicado_no_diario_oficial.prestacoes_de_conta_do_consolidado_dre.all()) == 1
+    assert len(consolidado_dre_publicado_no_diario_oficial_com_uma_pc_vinculada.prestacoes_de_conta_do_consolidado_dre.all()) == 1
 
     # Pcs vinculadas a retificação
     assert len(retificacao_dre.prestacoes_de_conta_do_consolidado_dre.all()) == 0
