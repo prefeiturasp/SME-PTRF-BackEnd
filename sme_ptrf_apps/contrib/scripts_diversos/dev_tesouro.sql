3c9fee560d4d4a3812c553907ef3a4ad0b201480
Lucas Santos Rocha <67693972+Lucas-Santos-Rocha-dev@users.noreply.github.com>
2023-03-13 14:51:19 -0300

    Fix/88450 acertos lancamento nao carrega (#1835)
    * fix(88450): Acertos lancamentos não carrega
    
    Cria ação no admin para criar solicitação de devolução ao tesouro para solicitacoes de acertos do tipo devolucao
    
    * fix(88450): Acertos lancamentos não carrega
    
    Cria ação no admin para criar solicitação de devolução ao tesouro para solicitacoes de acertos do tipo devolucao

--- sme_ptrf_apps/contrib/scripts_diversos/dev_tesouro.sql
+++ sme_ptrf_apps/contrib/scripts_diversos/dev_tesouro.sql
@@ -120,3 +120,99 @@ a.unidade_id,
 u.nome,
 p.referencia,
 d.id, d.numero_documento
+
+
+-- Lista solicitações de acerto do tipo devolução sem solicitação de devolução ao tesouro vinculada
+
+SELECT
+a.unidade_id as "codigo_eol",
+u.nome,
+pc.id as "pc_id",
+pc.status,
+p.referencia,
+d.id as "despesa_id", d.numero_documento, d.valor_total,
+sal.devolucao_ao_tesouro_id,
+sal.id as "solicitacao_acerto_id",
+apc.id as "analise_pc_id"
+
+FROM
+
+core_prestacaoconta as pc,
+core_periodo as p,
+core_associacao as a,
+core_unidade as u,
+core_analiseprestacaoconta as apc,
+core_analiselancamentoprestacaoconta as al,
+despesas_despesa as d,
+core_solicitacaoacertolancamento as sal,
+core_tipoacertolancamento as tal
+
+
+WHERE
+pc.periodo_id = p.id
+and a.unidade_id = u.codigo_eol
+and pc.associacao_id = a.id
+and apc.prestacao_conta_id = pc.id
+and al.analise_prestacao_conta_id = apc.id
+and al.despesa_id = d.id
+and sal.analise_lancamento_id = al.id
+and tal.id = sal.tipo_acerto_id
+and tal.categoria = 'DEVOLUCAO'
+and (select count(sdt.id) from core_solicitacaodevolucaoaotesouro as sdt where sdt.solicitacao_acerto_lancamento_id = sal.id) = 0
+and pc.status not in('APROVADA', 'APROVADA_RESSALVA', 'REPROVADA')
+
+order by
+u.nome,
+sal.id desc
+
+
+-- Lista solicitações de acerto do tipo devolução sem solicitação de devolução ao tesouro vinculada
+-- Agrupado por despesa
+
+SELECT
+a.unidade_id as "codigo_eol",
+u.nome,
+pc.id as "pc_id",
+pc.status,
+p.referencia,
+d.id as "despesa_id",
+d.numero_documento,
+d.valor_total
+
+FROM
+
+core_prestacaoconta as pc,
+core_periodo as p,
+core_associacao as a,
+core_unidade as u,
+core_analiseprestacaoconta as apc,
+core_analiselancamentoprestacaoconta as al,
+despesas_despesa as d,
+core_solicitacaoacertolancamento as sal,
+core_tipoacertolancamento as tal
+
+WHERE
+pc.periodo_id = p.id
+and a.unidade_id = u.codigo_eol
+and pc.associacao_id = a.id
+and apc.prestacao_conta_id = pc.id
+and al.analise_prestacao_conta_id = apc.id
+and al.despesa_id = d.id
+and sal.analise_lancamento_id = al.id
+and tal.id = sal.tipo_acerto_id
+and tal.categoria = 'DEVOLUCAO'
+and (select count(sdt.id) from core_solicitacaodevolucaoaotesouro as sdt where sdt.solicitacao_acerto_lancamento_id = sal.id) = 0
+and pc.status not in('APROVADA', 'APROVADA_RESSALVA', 'REPROVADA')
+
+group by
+a.unidade_id,
+u.nome,
+pc.id,
+pc.status,
+p.referencia,
+d.id,
+d.numero_documento,
+d.valor_total
+
+order by
+u.nome
