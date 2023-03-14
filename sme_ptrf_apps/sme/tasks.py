3fe22c4fa9f0e4b926368cca8ffef46af23b4733
Matheus de Castro Diori <44413101+mdiori@users.noreply.github.com>
2023-03-14 11:10:47 -0300

    feat(86738): Exportação de materiais e serviços (#1837)
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>

--- sme_ptrf_apps/sme/tasks.py
+++ sme_ptrf_apps/sme/tasks.py
@@ -1,9 +1,12 @@
 import logging
 
 from celery import shared_task
+from sme_ptrf_apps.despesas.models.especificacao_material_servico import EspecificacaoMaterialServico
+from sme_ptrf_apps.despesas.models.tipo_custeio import TipoCusteio
 from sme_ptrf_apps.receitas.models.receita import Receita
 from sme_ptrf_apps.sme.services.exporta_dados_creditos_service import ExportacoesDadosCreditosService
 from sme_ptrf_apps.dre.models import AnaliseConsolidadoDre
+from sme_ptrf_apps.sme.services.exporta_materiais_e_servicos_service import ExportacoesDadosMateriaisEServicosService
 
 
 logger = logging.getLogger(__name__)
@@ -42,6 +45,43 @@ def exportar_receitas_async(data_inicio, data_final, username):
 
     logger.info("Exportação csv finalizada com sucesso.")
 
+@shared_task(
+    retry_backoff=2,
+    retry_kwargs={'max_retries': 8},
+    time_limet=600,
+    soft_time_limit=300
+)
+def exportar_materiais_e_servicos_async(data_inicio, data_final, username):
+    logger.info("Exportando csv em processamento...")
+
+    queryset = EspecificacaoMaterialServico.objects.all()
+    queryset_tipo_custeio = TipoCusteio.objects.all()
+    try:
+        logger.info("Criando arquivo %s especificacoes_materiais_servicos.csv")
+        params = {
+            'queryset': queryset,
+            'data_inicio': data_inicio,
+            'data_final': data_final,
+            'user': username,
+        }
+
+        ExportacoesDadosMateriaisEServicosService(
+            **params,
+            nome_arquivo='especificacoes_materiais_servicos.csv'
+        ).exporta_materiais_e_servicos()
+
+        params['queryset'] = queryset_tipo_custeio
+        logger.info("Criando arquivo %s tipos_de_custeio.csv")
+        ExportacoesDadosMateriaisEServicosService(
+            **params,
+            nome_arquivo='tipos_de_custeio.csv'
+        ).exporta_tipos_de_custeio()
+        
+    except Exception as e:
+        logger.error(f"Erro ao exportar csv: {e}")
+        raise e
+
+    logger.info("Exportação csv finalizada com sucesso.")
 
 @shared_task(
     retry_backoff=2,
