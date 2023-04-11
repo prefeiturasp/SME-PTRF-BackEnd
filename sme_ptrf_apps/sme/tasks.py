4edf67b18729b415beaef7aa2e063b06e4519e0f
Matheus de Castro Diori <44413101+mdiori@users.noreply.github.com>
2023-04-11 07:26:31 -0300

    feat(90603): Adiciona a extração dos status das prestações de contas (#1915)
    Co-authored-by: Matheus <matheus.diori@amcom.com.br>

--- sme_ptrf_apps/sme/tasks.py
+++ sme_ptrf_apps/sme/tasks.py
@@ -3,10 +3,12 @@ import logging
 from celery import shared_task
 from sme_ptrf_apps.despesas.models.especificacao_material_servico import EspecificacaoMaterialServico
 from sme_ptrf_apps.despesas.models.tipo_custeio import TipoCusteio
+from sme_ptrf_apps.core.models.prestacao_conta import PrestacaoConta
 from sme_ptrf_apps.receitas.models.receita import Receita
 from sme_ptrf_apps.sme.services.exporta_dados_creditos_service import ExportacoesDadosCreditosService
 from sme_ptrf_apps.dre.models import AnaliseConsolidadoDre
 from sme_ptrf_apps.sme.services.exporta_materiais_e_servicos_service import ExportacoesDadosMateriaisEServicosService
+from sme_ptrf_apps.sme.services.exporta_status_prestacoes_conta_service import ExportacoesStatusPrestacoesContaService
 from sme_ptrf_apps.sme.services.exporta_saldo_final_periodo_pc_service import ExportacoesDadosSaldosFinaisPeriodoService
 from sme_ptrf_apps.sme.services.exporta_relacao_bens_pc import ExportacoesDadosRelacaoBensService
 from sme_ptrf_apps.core.models import FechamentoPeriodo, RelacaoBens
@@ -141,6 +143,37 @@ def exportar_relacao_bens_async(data_inicio, data_final, username):
             **params,
             nome_arquivo='pcs_relacoes_bens.csv'
         ).exporta_relacao_bens()
+    except Exception as e:
+        logger.error(f"Erro ao exportar csv: {e}")
+        raise e
+
+    logger.info("Exportação csv finalizada com sucesso.")
+
+@shared_task(
+    retry_backoff=2,
+    retry_kwargs={'max_retries': 8},
+    time_limet=600,
+    soft_time_limit=300
+)
+def exportar_status_prestacoes_contas_async(data_inicio, data_final, username):
+    logger.info("Exportando csv em processamento...")
+
+    queryset = PrestacaoConta.objects.all().order_by('criado_em')
+
+    try:
+        logger.info("Criando arquivo %s status_prestacoes_de_contas.csv")
+        params = {
+            'queryset': queryset,
+            'data_inicio': data_inicio,
+            'data_final': data_final,
+            'user': username,
+        }
+
+        ExportacoesStatusPrestacoesContaService(
+            **params,
+            nome_arquivo='status_prestacoes_de_contas.csv'
+        ).exporta_status_prestacoes_conta()
+
     except Exception as e:
         logger.error(f"Erro ao exportar csv: {e}")
         raise e
