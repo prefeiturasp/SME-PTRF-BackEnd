e4307fa354ceca2b5852d1dab302e704d1f796d1
Matheus <matheus.diori@amcom.com.br>
2023-04-12 10:05:02 -0300

    feat(90620): Adiciona extração dados devoluções ao tesouro

--- sme_ptrf_apps/sme/api/views/exportacoes_dados.py
+++ sme_ptrf_apps/sme/api/views/exportacoes_dados.py
@@ -7,8 +7,7 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 from sme_ptrf_apps.core.models.arquivos_download import ArquivoDownload
 from sme_ptrf_apps.sme.tasks import exportar_materiais_e_servicos_async, \
-    exportar_receitas_async, exportar_saldos_finais_periodo_async, exportar_relacao_bens_async, \
-    exportar_status_prestacoes_contas_async
+    exportar_receitas_async, exportar_saldos_finais_periodo_async, exportar_relacao_bens_async, exportar_status_prestacoes_contas_async, exportar_devolucoes_ao_tesouro_async
 from sme_ptrf_apps.users.permissoes import PermissaoAPIApenasSmeComLeituraOuGravacao
 
 logger = logging.getLogger(__name__)
@@ -94,6 +93,23 @@ class ExportacoesDadosViewSet(GenericViewSet):
     )
     def relacao_bens(self, request):
         exportar_relacao_bens_async.delay(
+            data_inicio=request.query_params.get('data_inicio'),
+            data_final=request.query_params.get('data_final'),
+            username=request.user.username
+        )
+
+        return Response(
+            {'response': "Arquivo gerado com sucesso, enviado para a central de download"},
+            status=HTTP_201_CREATED
+        )
+    
+    @action(
+        detail=False, methods=['get'],
+        url_path='devolucao-ao-tesouro-prestacoes-contas',
+        permission_classes=permission_classes
+    )
+    def devolucao_ao_tesouro_prestacao_conta(self, request):
+        exportar_devolucoes_ao_tesouro_async.delay(
             data_inicio=request.query_params.get('data_inicio'),
             data_final=request.query_params.get('data_final'),
             username=request.user.username
