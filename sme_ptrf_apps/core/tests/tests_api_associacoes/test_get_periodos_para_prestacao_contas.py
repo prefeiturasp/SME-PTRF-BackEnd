ce89941267ed6f74835e3bdb3ebf374385628b99
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-05-17 16:53:00 -0300

    fix(91768): Altera teste

--- sme_ptrf_apps/core/tests/tests_api_associacoes/test_get_periodos_para_prestacao_contas.py
+++ sme_ptrf_apps/core/tests/tests_api_associacoes/test_get_periodos_para_prestacao_contas.py
@@ -170,11 +170,21 @@ def test_get_periodos_prestacao_de_contas_ate_encerramento_da_associacao_com_pre
 @freeze_time('2020-06-15')
 def test_get_periodos_prestacao_de_contas_ate_encerramento_da_associacao_sem_prestacao_anterior(
     jwt_authenticated_client_a,
-    associacao_encerrada_2021_2
+    associacao_encerrada_2021_2,
+    periodo_2020_1
 ):
     response = jwt_authenticated_client_a.get(f'/api/associacoes/{associacao_encerrada_2021_2.uuid}/periodos-para-prestacao-de-contas/',
                           content_type='application/json')
     result = json.loads(response.content)
+
+    result_uuids = []
+    for _result in result:
+        result_uuids.append(_result['uuid'])
+
+    uuids_esperados = [
+        f'{periodo_2020_1.uuid}',
+    ]
+
     assert response.status_code == status.HTTP_200_OK
 
-    assert result == []
+    assert uuids_esperados == result_uuids
