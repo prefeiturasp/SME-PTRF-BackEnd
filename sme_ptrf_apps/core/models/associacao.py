98620e0bc5445c2b212d571d3cf0903ce152d105
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-05-18 09:37:34 -0300

    fix(91768): Corrige filtro periodo seguinte

--- sme_ptrf_apps/core/models/associacao.py
+++ sme_ptrf_apps/core/models/associacao.py
@@ -190,14 +190,17 @@ class Associacao(ModeloIdNome):
         ultima_prestacao_feita = prestacoes_da_associacao.last()
         ultimo_periodo_com_prestacao = ultima_prestacao_feita.periodo if ultima_prestacao_feita else None
         if ultimo_periodo_com_prestacao:
-            return ultimo_periodo_com_prestacao.periodo_seguinte.first()
+            periodo_seguinte = ultimo_periodo_com_prestacao.periodo_seguinte.first()
+            if not self.encerrada or (self.encerrada and periodo_seguinte.data_inicio_realizacao_despesas <= self.data_de_encerramento):
+                return periodo_seguinte
+            else:
+                return None
         else:
             return self.periodo_inicial.periodo_seguinte.first() if self.periodo_inicial else None
 
     def periodos_para_prestacoes_de_conta(self, ignorar_devolvidas=False):
         periodos = set(
             self.periodos_com_prestacao_de_contas(ignorar_pcs_com_acertos_que_demandam_exclusoes_e_fechamentos=True))
-
 
         proximo_periodo = self.proximo_periodo_de_prestacao_de_contas(ignorar_devolvidas)
         if proximo_periodo:
