e4f59b551abc5eb85e2c3a7fc38acc8272fd6426
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-07-07 13:18:43 -0300

    feat(85375): Add filtros por nome e visão na view de grupos
    Altera a visão de grupos para permitir filtrar por
    visão (id, nome) e um search por name e descrição.

--- sme_ptrf_apps/users/api/views/grupos_viewset.py
+++ sme_ptrf_apps/users/api/views/grupos_viewset.py
@@ -1,8 +1,8 @@
+from django_filters import rest_framework as filters
 from rest_framework import mixins
-
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.filters import SearchFilter
 
 from ..serializers import GrupoSerializer
 from ...models import Grupo
@@ -15,3 +15,6 @@ class GruposViewSet(mixins.ListModelMixin, GenericViewSet):
     lookup_field = 'id'
     queryset = Grupo.objects.all().order_by('name')
     serializer_class = GrupoSerializer
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter,)
+    search_fields = ['name', 'descricao', ]
+    filter_fields = ('visoes__id', 'visoes__nome')
