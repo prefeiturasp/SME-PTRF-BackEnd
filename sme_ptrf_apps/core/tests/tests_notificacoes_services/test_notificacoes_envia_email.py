4db5d2e644c67b2a51874f8946fc9498e66bde2d
Matheus <matheus.diori@amcom.com.br>
2023-08-30 14:45:32 -0300

    feat(100876): Adiciona mock de variavel de ambiente

--- sme_ptrf_apps/core/tests/tests_notificacoes_services/test_notificacoes_envia_email.py
+++ sme_ptrf_apps/core/tests/tests_notificacoes_services/test_notificacoes_envia_email.py
@@ -5,8 +5,9 @@ from ....core.models import Notificacao
 
 pytestmark = pytest.mark.django_db
 
-
-def test_notificar_deve_enviar_email(usuario_notificavel):
+def test_notificar_deve_enviar_email(usuario_notificavel, monkeypatch):
+    monkeypatch.setenv('SERVER_NAME', 'sig-escola.sme.prefeitura.sp.gov.br')
+    
     path = 'sme_ptrf_apps.core.services.notificacao_services.enviar_email_notificacao.enviar_email_nova_notificacao'
 
     with patch(path) as mock_enviar_email:
@@ -19,8 +20,24 @@ def test_notificar_deve_enviar_email(usuario_notificavel):
             usuario=usuario_notificavel,
             renotificar=False,
             enviar_email=True,
-            testing=True
         )
         mock_enviar_email.assert_called_once_with(usuario=usuario_notificavel, titulo='Notificacao teste', descricao="Descricao teste")
 
 
+def test_nao_notificar_deve_enviar_email_se_estiver_fora_de_ambiente_de_producao(usuario_notificavel, monkeypatch):
+    monkeypatch.setenv('SERVER_NAME', 'um_server_name_qualquer')
+    
+    path = 'sme_ptrf_apps.core.services.notificacao_services.enviar_email_notificacao.enviar_email_nova_notificacao'
+
+    with patch(path) as mock_enviar_email:
+        Notificacao.notificar(
+            tipo=Notificacao.TIPO_NOTIFICACAO_ALERTA,
+            categoria=Notificacao.CATEGORIA_NOTIFICACAO_ANALISE_PC,
+            remetente=Notificacao.REMETENTE_NOTIFICACAO_SISTEMA,
+            titulo=f"Notificacao teste",
+            descricao=f"Descricao teste",
+            usuario=usuario_notificavel,
+            renotificar=False,
+            enviar_email=True,
+        )
+        mock_enviar_email.assert_not_called()
