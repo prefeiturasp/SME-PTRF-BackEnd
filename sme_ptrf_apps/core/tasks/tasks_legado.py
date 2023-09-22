c2b842b5114162b5ae9967e1d1132e5c381336d9
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-09-22 15:10:00 -0300

    refac(91026): Extrai task gerar_notificacao_proximidade_fim_periodo_prestacao_conta_async

--- sme_ptrf_apps/core/tasks/tasks_legado.py
+++ sme_ptrf_apps/core/tasks/tasks_legado.py
@@ -261,22 +261,22 @@ logger = logging.getLogger(__name__)
 #     notificar_pendencia_envio_prestacao_de_contas()
 #
 #     logger.info(f'Finalizando a geração de notificação pendência envio prestação de contas async')
-
-
-@shared_task(
-    retry_backoff=2,
-    retry_kwargs={'max_retries': 8},
-    time_limet=600,
-    soft_time_limit=300
-)
-def gerar_notificacao_proximidade_inicio_periodo_prestacao_conta_async():
-    from sme_ptrf_apps.core.services.notificacao_services import notificar_proximidade_inicio_periodo_prestacao_conta
-
-    logger.info('Chamando serviço de notificação de proximidade do início do período de prestação de contas async.')
-
-    notificar_proximidade_inicio_periodo_prestacao_conta()
-
-    logger.info('Executado serviço de notificação de proximidade do início do período de prestação de contas async.')
+#
+#
+# @shared_task(
+#     retry_backoff=2,
+#     retry_kwargs={'max_retries': 8},
+#     time_limet=600,
+#     soft_time_limit=300
+# )
+# def gerar_notificacao_proximidade_inicio_periodo_prestacao_conta_async():
+#     from sme_ptrf_apps.core.services.notificacao_services import notificar_proximidade_inicio_periodo_prestacao_conta
+#
+#     logger.info('Chamando serviço de notificação de proximidade do início do período de prestação de contas async.')
+#
+#     notificar_proximidade_inicio_periodo_prestacao_conta()
+#
+#     logger.info('Executado serviço de notificação de proximidade do início do período de prestação de contas async.')
 
 
 @shared_task(
