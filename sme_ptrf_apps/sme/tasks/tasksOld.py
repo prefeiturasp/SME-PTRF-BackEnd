20be1a64a2d78d86cff940a212415b270f9f5ba3
Matheus <matheus.diori@amcom.com.br>
2023-10-16 13:31:03 -0300

    refac(104325): Extrai exportar_devolucoes_ao_tesouro_async

--- sme_ptrf_apps/sme/tasks/tasksOld.py
+++ sme_ptrf_apps/sme/tasks/tasksOld.py
@@ -212,7 +212,7 @@ def exportar_status_prestacoes_contas_async(data_inicio, data_final, username):
 
     logger.info("Exportação csv finalizada com sucesso.") """
 
-@shared_task(
+""" @shared_task(
     retry_backoff=2,
     retry_kwargs={'max_retries': 8},
     time_limet=600,
@@ -241,7 +241,7 @@ def exportar_devolucoes_ao_tesouro_async(data_inicio, data_final, username):
         logger.error(f"Erro ao exportar csv: {e}")
         raise e
 
-    logger.info("Exportação csv finalizada com sucesso.")
+    logger.info("Exportação csv finalizada com sucesso.") """
 
 @shared_task(
     retry_backoff=2,
