e05c972fbafa0663451d1b970c5eb4b1816b1fde
Alessandro Fernandes <alcfernandes@yahoo.com>
2023-12-27 13:48:24 -0300

    feat(110921): Inclui CALCULADA na consulta de status da PC

--- sme_ptrf_apps/core/services/periodo_services.py
+++ sme_ptrf_apps/core/services/periodo_services.py
@@ -55,7 +55,8 @@ def status_prestacao_conta_associacao(periodo_uuid, associacao_uuid):
         PrestacaoConta.STATUS_APROVADA_RESSALVA: 'Prestação de contas aprovada com ressalvas pela DRE.',
         PrestacaoConta.STATUS_REPROVADA: 'Prestação de contas reprovada pela DRE.',
         PrestacaoConta.STATUS_EM_PROCESSAMENTO: 'Documentos em processamento.',
-        PrestacaoConta.STATUS_A_PROCESSAR: 'PC aguardando processamento.'
+        PrestacaoConta.STATUS_A_PROCESSAR: 'PC aguardando processamento.',
+        PrestacaoConta.STATUS_CALCULADA: 'PC calculada. Gerando relatórios...'
     }
 
     STATUS_PERIODO_EM_ANDAMENTO = 'PERIODO_EM_ANDAMENTO'
@@ -72,7 +73,8 @@ def status_prestacao_conta_associacao(periodo_uuid, associacao_uuid):
         PrestacaoConta.STATUS_APROVADA_RESSALVA: 5,
         PrestacaoConta.STATUS_REPROVADA: 3,
         PrestacaoConta.STATUS_EM_PROCESSAMENTO: 6,
-        PrestacaoConta.STATUS_A_PROCESSAR: 6
+        PrestacaoConta.STATUS_A_PROCESSAR: 6,
+        PrestacaoConta.STATUS_CALCULADA: 6,
     }
 
     mensagem_periodo = 'Período finalizado.' if periodo.encerrado else 'Período em andamento.'
