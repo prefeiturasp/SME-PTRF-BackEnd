fed4278b0b01085bc0d33941313ed0739313344f
Alessandro Fernandes <alcfernandes@yahoo.com>
2024-02-06 09:19:26 -0300

    feat(113127): ContextualLogger no processo de PC básico
    Implementa o uso do ContextualLogger no processo de
    geração de uma prestação de contas.

--- sme_ptrf_apps/core/tasks/terminar_processo_pc.py
+++ sme_ptrf_apps/core/tasks/terminar_processo_pc.py
@@ -1,10 +1,7 @@
-import logging
-
-
 from celery import shared_task
 
-
-logger = logging.getLogger(__name__)
+from sme_ptrf_apps.core.models import TaskCelery
+from sme_ptrf_apps.logging.loggers import ContextualLogger
 
 
 @shared_task(
@@ -17,16 +14,31 @@ def terminar_processo_pc_async(
     periodo_uuid,
     associacao_uuid,
     username="",
+    id_task=None,
 ):
     from sme_ptrf_apps.core.services.prestacao_conta_service import PrestacaoContaService
 
-    logger.info(f'Iniciando a task terminar_processo_pc_async')
+    logger_terminar_pc = ContextualLogger.get_logger(
+        __name__,
+        operacao='Prestação de Contas',
+        username=username,
+        task_id=str(id_task),
+    )
+
+    task = TaskCelery.objects.get(uuid=id_task)
+
+    # Apenas para informar que os logs não mais ficarão registrados na task.
+    task.grava_log_concatenado('Iniciando a task terminar_processo_pc_async. Logs registrados apenas no Kibana.')
 
     pc_service = PrestacaoContaService(
         periodo_uuid=periodo_uuid,
         associacao_uuid=associacao_uuid,
         username=username,
+        logger=logger_terminar_pc
     )
 
+    logger_terminar_pc.info(f'Iniciando a task terminar_processo_pc_async.')
     pc_service.terminar_processo_pc()
+    logger_terminar_pc.info(f'Task terminar_processo_pc_async finalizada com sucesso.')
 
+    task.registra_data_hora_finalizacao('Task terminar_processo_pc_async finalizada com sucesso.')
