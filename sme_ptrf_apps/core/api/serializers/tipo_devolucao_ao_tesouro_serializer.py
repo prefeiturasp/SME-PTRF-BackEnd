2bd1e765a7c23b4dbcaabecde3046fe28ecf8aae
Agostinho <jr.jardimmatos@gmail.com>
2025-01-27 14:12:49 -0300

    feature(124987): API para Tipos de Devolucao ao Tesouro

--- sme_ptrf_apps/core/api/serializers/tipo_devolucao_ao_tesouro_serializer.py
+++ sme_ptrf_apps/core/api/serializers/tipo_devolucao_ao_tesouro_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from sme_ptrf_apps.utils.update_instance_from_dict import update_instance_from_dict
 from ...models import TipoDevolucaoAoTesouro
 
 
@@ -7,3 +7,24 @@ class TipoDevolucaoAoTesouroSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoDevolucaoAoTesouro
         fields = ('id', 'nome', 'uuid')
+
+    def create(self, validated_data):
+        nome = validated_data['nome']
+        nome_ja_cadastrado = TipoDevolucaoAoTesouro.objects.filter(nome__iexact=nome).exists()
+        if nome_ja_cadastrado:
+            raise serializers.ValidationError(
+                {'non_field_errors': 'Este motivo de devolução ao tesouro já existe.'})
+
+        motivo_devolucao_criado = TipoDevolucaoAoTesouro.objects.create(**validated_data)
+        return motivo_devolucao_criado
+
+    def update(self, instance, validated_data):
+        nome = validated_data.get('nome')
+        if nome and instance.nome != nome:
+            nome_ja_cadastrado = TipoDevolucaoAoTesouro.objects.filter(nome__iexact=nome).exists()
+
+            if nome_ja_cadastrado:
+                raise serializers.ValidationError(
+                    {"non_field_errors": "Este motivo de devolução ao tesouro já existe."})
+        update_instance_from_dict(instance, validated_data, save=True)
+        return instance
