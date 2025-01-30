4467e5a47e4246afb3dc7ed058a009bbe7ecde00
Vitor Campos <vitor.campos@spassu.com.br>
2025-01-30 10:14:00 -0300

    feature(124909): Adição de filtro por motivo no viewset de motivos de aprovacao com ressalva. Adição de validação na criação de edição do serializer para não ter registros com mesmo motivo

--- sme_ptrf_apps/dre/api/serializers/motivo_aprovacao_ressalva_serializer.py
+++ sme_ptrf_apps/dre/api/serializers/motivo_aprovacao_ressalva_serializer.py
@@ -6,4 +6,28 @@ from ...models import MotivoAprovacaoRessalva
 class MotivoAprovacaoRessalvaSerializer(serializers.ModelSerializer):
     class Meta:
         model = MotivoAprovacaoRessalva
-        fields = ('uuid', 'motivo')
+        fields = ('id', 'uuid', 'motivo')
+
+
+    def create(self, validated_data):
+        motivo = validated_data.get('motivo')
+
+        if MotivoAprovacaoRessalva.objects.filter(motivo=motivo).exists():
+            raise serializers.ValidationError({
+                'non_field_errors': 'Este motivo de aprovação de pc com ressalva já existe.'
+                })
+
+        instance = super().create(validated_data)
+        return instance
+
+
+    def update(self, instance, validated_data):
+        motivo = validated_data.get('motivo')
+
+        if MotivoAprovacaoRessalva.objects.filter(motivo=motivo).exclude(pk=self.instance.pk).exists():
+            raise serializers.ValidationError({
+                'non_field_errors': 'Este motivo de aprovação de pc com ressalva já existe.'
+                })
+
+        instance = super().update(instance, validated_data)
+        return instance
