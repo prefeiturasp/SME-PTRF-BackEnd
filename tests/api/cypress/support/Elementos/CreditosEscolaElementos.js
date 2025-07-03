fbb4ff6ef2af8826512759f5431f4fbe7f186728
Leandro S Castro <leandro2509@gmail.com>
2025-07-03 12:13:30 -0300

    Ajustes, correções e refactor no cenários de crédito escola

--- tests/api/cypress/support/Elementos/CreditosEscolaElementos.js
+++ tests/api/cypress/support/Elementos/CreditosEscolaElementos.js
@@ -1,110 +1,263 @@
 class CreditosEscolaElementos {
+  //----------------------- Selecionar Tipos de Receita -------------------------\\
+  arredondamento = () => {
+    return cy
+      .get(".d-flex > #tipo_receita")
+      .select("Arredondamento na transferência de saldo entre cartões BB");
+  };
+  devolucaoConta = () => {
+    return cy.get(".d-flex > #tipo_receita").select("Devolução à conta");
+  };
+  estorno = () => {
+    return cy.get(".d-flex > #tipo_receita").select("Estorno");
+  };
+  recursoExterno = () => {
+    return cy.get(".d-flex > #tipo_receita").select("Recurso Externo");
+  };
+  rendimento = () => {
+    return cy.get(".d-flex > #tipo_receita").select("Rendimento");
+  };
+  repasse = () => {
+    return cy.get(".d-flex > #tipo_receita").select("Repasse");
+  };
 
-    //----------------------- Selecionar Tipos de Receita -------------------------\\    
-    arredondamento = () => { return cy.get('.d-flex > #tipo_receita').select("Arredondamento na transferência de saldo entre cartões BB")};
-    devolucaoConta = () => { return cy.get('.d-flex > #tipo_receita').select("Devolução à conta")};
-    estorno = () => { return cy.get('.d-flex > #tipo_receita').select("Estorno")};
-    recursoExterno = () => { return cy.get('.d-flex > #tipo_receita').select("Recurso Externo")};
-    rendimento = () => { return cy.get('.d-flex > #tipo_receita').select("Rendimento")};
-    repasse = () => { return cy.get('.d-flex > #tipo_receita').select("Repasse")};
+  //----------------------- Botões -------------------------\\
+  botaoFiltrarReceita = () => {
+    return cy.get(".form-inline > .d-flex > .btn").contains("Filtrar");
+  };
+  botaoMaisFiltros = () => {
+    return cy.get(".col-md-2 > .btn").contains("Mais Filtros");
+  };
+  botaoFiltrarMaisFiltros = () => {
+    return cy
+      .get(":nth-child(1) > form > .d-flex > .btn-success")
+      .contains("Filtrar");
+  };
+  botaoLimparFiltros = () => {
+    return cy.get(".btn-outline-success.ml-2").contains("Limpar Filtros");
+  };
+  botaoCadastrarCredito = () => {
+    return cy
+      .get(".col-md-5.mt-2 > :nth-child(1)")
+      .contains("Cadastrar crédito");
+  };
+  botaoValoresReprogramados = () => {
+    return cy.get(".col-md-5.mt-2 > .mr-2").contains("Valores reprogramados");
+  };
+  botaoSalvarCadastroCredito = () => {
+    return cy.get(".btn-success").contains("Salvar");
+  };
+  botaoCancelar = () => {
+    return cy
+      .get(":nth-child(1) > form > .d-flex > :nth-child(1)")
+      .contains("Cancelar");
+  };
+  botaoDeletarCadastroCredito = () => {
+    return cy.get(".btn-danger").contains("Deletar");
+  };
+  botaoVoltarCadastroCredito = () => {
+    return cy.get(".btn-outline-success").contains("Voltar");
+  };
+  botaoNaoCancelarRepasse = () => {
+    return cy.get(".modal-footer > .btn-outline-success");
+  };
+  botaoGravarRepasse = () => {
+    return cy.get(".modal-footer > .btn-success");
+  };
 
+  //----------------------- Mais Filtros -------------------------\\
+  arredondamentoMaisFiltros = () => {
+    return cy
+      .get(".form-row > :nth-child(1) > #tipo_receita")
+      .select("Arredondamento na transferência de saldo entre cartões BB");
+  };
+  devolucaoContaMaisFiltros = () => {
+    return cy
+      .get(".form-row > :nth-child(1) > #tipo_receita")
+      .select("Devolução à conta");
+  };
+  estornoMaisFiltros = () => {
+    return cy
+      .get(".form-row > :nth-child(1) > #tipo_receita")
+      .select("Estorno");
+  };
+  recursoExternoMaisFiltros = () => {
+    return cy
+      .get(".form-row > :nth-child(1) > #tipo_receita")
+      .select("Rendimento");
+  };
+  rendimentoMaisFiltros = () => {
+    return cy
+      .get(".form-row > :nth-child(1) > #tipo_receita")
+      .select("Rendimento");
+  };
+  repasseMaisFiltros = () => {
+    return cy
+      .get(".form-row > :nth-child(1) > #tipo_receita")
+      .select("Repasse");
+  };
+  selecionarDetalhamentoCreditoMaisFiltros = () => {
+    return cy.get("#filtrar_por_termo");
+  };
 
-    //----------------------- Botões -------------------------\\
-    botaoFiltrarReceita = () => { return cy.get('.form-inline > .d-flex > .btn').contains('Filtrar')};
-    botaoMaisFiltros = () => { return cy.get('.col-md-2 > .btn').contains('Mais Filtros')};
-    botaoFiltrarMaisFiltros = () => { return cy.get(':nth-child(1) > form > .d-flex > .btn-success').contains('Filtrar')};
-    botaoLimparFiltros = () => { return cy.get('.btn-outline-success.ml-2').contains('Limpar Filtros')};
-    botaoCadastrarCredito = () => { return cy.get('.col-md-5.mt-2 > :nth-child(1)').contains('Cadastrar crédito')};
-    botaoValoresReprogramados = () => { return cy.get('.col-md-5.mt-2 > .mr-2').contains('Valores reprogramados')};
-    botaoSalvarCadastroCredito = () => { return cy.get('.btn-success').contains('Salvar')};
-    botaoCancelar = () => { return cy.get(':nth-child(1) > form > .d-flex > :nth-child(1)').contains('Cancelar')};
-    botaoDeletarCadastroCredito = () => { return cy.get('.btn-danger').contains('Deletar')};    
-    botaoVoltarCadastroCredito = () => { return cy.get('.btn-outline-success').contains('Voltar')};   
-    botaoNaoCancelarRepasse = () => { return cy.get('.modal-footer > .btn-outline-success')};
-    botaoGravarRepasse = () => { return cy.get('.modal-footer > .btn-success')};
+  //----------------------- Colunas dos Créditos Cadastrados -------------------------\\
+  validarTipo = () => {
+    return cy.get(".p-datatable-thead tr").contains("Tipo");
+  };
+  validarConta = () => {
+    return cy.get(".p-datatable-thead tr").contains("Conta");
+  };
+  validarAcao = () => {
+    return cy.get(".p-datatable-thead tr").contains("Ação");
+  };
+  validarData = () => {
+    return cy.get(".p-datatable-thead tr").contains("Data");
+  };
+  validarValor = () => {
+    return cy.get(".p-datatable-thead tr").contains("Valor");
+  };
 
+  //----------------------- Cadastro de Crédito -------------------------\\
+  selecionarDetalhamentoCredito = () => {
+    return cy.get("#detalhe_tipo_receita").select("Julho");
+  };
+  selecionarDataCredito = () => {
+    return cy.get(".react-datepicker__input-container > .form-control");
+  };
+  selecionarDataCreditoCalendario = () => {
+    return cy.get(".react-datepicker__month-container");
+  };
+  selecionarTipoContaCheque = () => {
+    return cy.get("#conta_associacao").select("Cheque");
+  };
+  selecionarTipoContaCartao = () => {
+    return cy.get("#conta_associacao").select("Cartão");
+  };
+  selecionarValorTotalCredito = () => {
+    return cy.get("#valor");
+  };
 
-    //----------------------- Mais Filtros -------------------------\\
-    arredondamentoMaisFiltros = () => { return cy.get('.form-row > :nth-child(1) > #tipo_receita').select("Arredondamento na transferência de saldo entre cartões BB")};
-    devolucaoContaMaisFiltros = () => { return cy.get('.form-row > :nth-child(1) > #tipo_receita').select("Devolução à conta")};
-    estornoMaisFiltros = () => { return cy.get('.form-row > :nth-child(1) > #tipo_receita').select("Estorno")};
-    recursoExternoMaisFiltros = () => { return cy.get('.form-row > :nth-child(1) > #tipo_receita').select("Recurso Externo")};
-    rendimentoMaisFiltros = () => { return cy.get('.form-row > :nth-child(1) > #tipo_receita').select("Rendimento")};
-    repasseMaisFiltros = () => { return cy.get('.form-row > :nth-child(1) > #tipo_receita').select("Repasse")};
-    selecionarDetalhamentoCreditoMaisFiltros = () => { return cy.get('#filtrar_por_termo')};
-     
+  //----------------------- Cadastro de Crédito (Ação) -------------------------\\
+  selecionarAcaoPtrf = () => {
+    return cy.get("#acao_associacao").select("PTRF Básico");
+  };
+  selecionarAcaoSalas = () => {
+    return cy.get("#acao_associacao").select("Salas e Espaços de Leitura");
+  };
+  selecionarAcaoMaterialPedagogico = () => {
+    return cy.get("#acao_associacao").select("Material Pedagógico");
+  };
+  selecionarAcaoRecurso = () => {
+    return cy.get("#acao_associacao").select("Recurso Externo");
+  };
+  selecionarAcaoMaterialComplementar = () => {
+    return cy.get("#acao_associacao").select("Material Complementar");
+  };
+  selecionarDataIncio = () => {
+    return cy.get(
+      ".pr-0 > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control"
+    );
+  };
+  selecionarDataFim = () => {
+    return cy.get(
+      ".pl-0 > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control"
+    );
+  };
 
-    //----------------------- Colunas dos Créditos Cadastrados -------------------------\\
-    validarTipo = () => { return cy.get('.p-datatable-thead > tr > :nth-child(1)').contains('Tipo')};
-    validarConta = () => { return cy.get('.p-datatable-thead > tr > :nth-child(2)').contains('Conta')};
-    validarAcao = () => { return cy.get('.p-datatable-thead > tr > :nth-child(3)').contains('Ação')};
-    validarData = () => { return cy.get('.p-datatable-thead > tr > :nth-child(4)').contains('Data')};
-    validarValor = () => { return cy.get('.p-datatable-thead > tr > :nth-child(5)').contains('Valor')};
+  //----------------------- Classificação do Crédito -------------------------\\
+  selecionarClassificacaoCreditoCusteio = () => {
+    return cy.get("#categoria_receita").select("Custeio");
+  };
+  selecionarClassificacaoCreditoLivreAplicacao = () => {
+    return cy.get("#categoria_receita").select("Livre Aplicação");
+  };
 
+  //----------------------- Tipos do Crédito -------------------------\\
+  selecionarReceitaRendimento = () => {
+    return cy.get("#tipo_receita").select("Rendimento");
+  };
+  selecionarReceitaRepasse = () => {
+    return cy.get("#tipo_receita").select("Repasse");
+  };
 
-    //----------------------- Cadastro de Crédito -------------------------\\
-    selecionarDetalhamentoCredito = () => { return cy.get('#detalhe_tipo_receita').select('Julho')};    
-    selecionarDataCredito = () => { return cy.get('.react-datepicker__input-container > .form-control')};
-    selecionarDataCreditoCalendario = () => { return cy.get('.react-datepicker__month-container')};
-    selecionarTipoContaCheque = () => { return cy.get('#conta_associacao').select('Cheque')};
-    selecionarTipoContaCartao = () => { return cy.get('#conta_associacao').select('Cartão')};
-    selecionarValorTotalCredito = () => { return cy.get('#valor')};
+  //----------------------- Grid Repasse -------------------------\\
+  selecionarValorCapital = () => {
+    return cy.contains("11.500");
+  };
+  selecionarValorCusteio = () => {
+    return cy.contains("8.600");
+  };
+  selecionarValorLivreAplicacao = () => {
+    return cy.contains("8.500");
+  };
 
+  //----------------------- Mensagens Campos Obrigatórios -------------------------\\
+  validaDetalhamento = () => {
+    return cy
+      .get(":nth-child(2) > .span_erro")
+      .contains("Detalhamento do crédito é obrigatório");
+  };
+  validaDataCredito = () => {
+    return cy
+      .get(":nth-child(3) > .span_erro")
+      .contains("Data do crédito é obrigatório.");
+  };
+  validaTipoConta = () => {
+    return cy
+      .get(":nth-child(4) > .span_erro")
+      .contains("Tipo de conta é obrigatório.");
+  };
+  validaAcaoSelecionada = () => {
+    return cy.get(":nth-child(5) > .span_erro").contains("Ação é obrigatório.");
+  };
+  validaClassificacaoSelecionada = () => {
+    return cy
+      .get(":nth-child(6) > .span_erro")
+      .contains("Classificação do crédito é obrigatório.");
+  };
+  validaCampoValor = () => {
+    return cy
+      .get(":nth-child(7) > .span_erro")
+      .contains("Valor do crédito é obrigatório.");
+  };
 
+  //----------------------- Edição Cadastro de Crédito -------------------------\\
+  editarRendimento = () => {
+    return cy
+      .get(".p-datatable-tbody > :nth-child(1) > :nth-child(1)")
+      .contains("Rendimento");
+  };
+  editarRepasse = () => {
+    return cy
+      .get(".p-datatable-tbody > :nth-child(1) > :nth-child(1)")
+      .contains("Repasse");
+  };
 
-    //----------------------- Cadastro de Crédito (Ação) -------------------------\\
-    selecionarAcaoPtrf = () => { return cy.get('#acao_associacao').select('PTRF Básico')};
-    selecionarAcaoSalas = () => { return cy.get('#acao_associacao').select('Salas e Espaços de Leitura')};
-    selecionarAcaoMaterialPedagogico = () => { return cy.get('#acao_associacao').select('Material Pedagógico')};
-    selecionarAcaoRecurso = () => { return cy.get('#acao_associacao').select('Recurso Externo')};
-    selecionarAcaoMaterialComplementar = () => { return cy.get('#acao_associacao').select('Material Complementar')};
-    selecionarDataIncio = () => { return cy.get('.pr-0 > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control')};
-    selecionarDataFim = () => { return cy.get('.pl-0 > .react-datepicker-wrapper > .react-datepicker__input-container > .form-control')};
+  //----------------------- Validar Créditos da Escola -------------------------\\
+  avancarPaginas = () => {
+    return cy.get(".p-paginator-next");
+  };
+  validarTabelaValor = () => {
+    return cy.get(".p-datatable-tbody tr");
+  };
+  recebeValor = () => {
+    return cy.get("td.py-3").find("span");
+  };
+  validarValorTotalFiltroAplicado = () => {
+    return cy.get(".table > tbody > tr > :nth-child(2)");
+  };
+  validarSemFiltroAplicado = () => {
+    return cy.get(".table > thead > tr > :nth-child(1)");
+  };
 
-
-    //----------------------- Classificação do Crédito -------------------------\\
-    selecionarClassificacaoCreditoCusteio = () => { return cy.get('#categoria_receita').select('Custeio')};
-    selecionarClassificacaoCreditoLivreAplicacao = () => { return cy.get('#categoria_receita').select('Livre Aplicação')};
-
-
-    //----------------------- Tipos do Crédito -------------------------\\
-    selecionarReceitaRendimento = () => { return cy.get('#tipo_receita').select('Rendimento')};
-    selecionarReceitaRepasse = () => { return cy.get('#tipo_receita').select('Repasse')};
-
-
-    //----------------------- Grid Repasse -------------------------\\
-    selecionarValorCapital = () => { return cy.contains('11.500')};
-    selecionarValorCusteio = () => { return cy.contains('8.600')};
-    selecionarValorLivreAplicacao = () => { return cy.contains('8.500')};
-
-
-    //----------------------- Mensagens Campos Obrigatórios -------------------------\\
-    validaDetalhamento = () => { return cy.get(':nth-child(2) > .span_erro').contains('Detalhamento do crédito é obrigatório')};
-    validaDataCredito = () => { return cy.get(':nth-child(3) > .span_erro').contains('Data do crédito é obrigatório.')};
-    validaTipoConta = () => { return cy.get(':nth-child(4) > .span_erro').contains('Tipo de conta é obrigatório.')};
-    validaAcaoSelecionada = () => { return cy.get(':nth-child(5) > .span_erro').contains('Ação é obrigatório.')};
-    validaClassificacaoSelecionada = () => { return cy.get(':nth-child(6) > .span_erro').contains('Classificação do crédito é obrigatório.')};
-    validaCampoValor = () => { return cy.get(':nth-child(7) > .span_erro').contains('Valor do crédito é obrigatório.')};
-        
-    
-
-    //----------------------- Edição Cadastro de Crédito -------------------------\\
-    editarRendimento = () => { return cy.get('.p-datatable-tbody > :nth-child(1) > :nth-child(1)').contains('Rendimento')};
-    editarRepasse = () => { return cy.get('.p-datatable-tbody > :nth-child(1) > :nth-child(1)').contains('Repasse')};
-
-
-    //----------------------- Validar Créditos da Escola -------------------------\\
-    avancarPaginas = () => { return cy.get('.p-paginator-next')};
-    validarTabelaValor = () => { return cy.get('.p-datatable-tbody tr')};
-    recebeValor = () => { return cy.get('td.py-3').find('span')};
-    validarValorTotalFiltroAplicado = () => { return cy.get('.table > tbody > tr > :nth-child(2)')};
-    validarSemFiltroAplicado = () => { return cy.get('.table > thead > tr > :nth-child(1)')};
-
-
-    //----------------------- Retorno de Mensagens apresentadas em tela -------------------------\\
-    semResultado = () => { return cy.get('.texto-404').contains('Não encontramos resultados, verifique os filtros e tente novamente.')};
-
-
+  //----------------------- Retorno de Mensagens apresentadas em tela -------------------------\\
+  semResultado = () => {
+    return cy
+      .get(".texto-404")
+      .contains(
+        "Não encontramos resultados, verifique os filtros e tente novamente."
+      );
+  };
 }
 
 export default CreditosEscolaElementos;
