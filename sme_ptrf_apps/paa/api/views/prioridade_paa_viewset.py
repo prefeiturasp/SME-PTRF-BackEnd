c4ddec33a120774bbc7802bd9c7f3ac66affefea
Agostinho <jr.jardimmatos@gmail.com>
2025-08-06 09:38:56 -0300

    feature(127467): Implementa exclusão em lote de prioridades do PAA

--- sme_ptrf_apps/paa/api/views/prioridade_paa_viewset.py
+++ sme_ptrf_apps/paa/api/views/prioridade_paa_viewset.py
@@ -2,6 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from django.http import Http404
 
 import django_filters
 from waffle.mixins import WaffleFlagMixin
@@ -14,7 +16,7 @@ from sme_ptrf_apps.paa.api.serializers import (
     PrioridadePaaCreateUpdateSerializer,
     PrioridadePaaListSerializer
 )
-from sme_ptrf_apps.users.permissoes import PermissaoApiUe
+from sme_ptrf_apps.users.permissoes import PermissaoApiUe, PermissaoAPITodosComGravacao
 from sme_ptrf_apps.paa.querysets import queryset_prioridades_paa
 
 
@@ -61,3 +63,56 @@ class PrioridadePaaViewSet(WaffleFlagMixin, ModelViewSet):
         )
 
         return Response(tabelas, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['post'], url_path='excluir-lote',
+            permission_classes=[PermissaoApiUe & PermissaoAPITodosComGravacao])
+    def excluir_em_lote(self, request, *args, **kwargs):
+        """
+        Exclui em lote as prioridades de PAA.
+
+        Essa action pode ser usada para excluir em lote as prioridades de PAA.
+
+        - lista_uuids: lista de uuids das prioridades a serem excluídas.
+
+        Retorna um dicionário com as informações dos erros e a mensagem
+        de sucesso ou erro.
+        """
+        lista_uuids = request.data.get('lista_uuids', [])
+
+        if not len(lista_uuids):
+            content = {
+                'erro': 'Falta de informações',
+                'mensagem': 'É necessário enviar a lista de uuids a serem excluídos (lista_uuids).'
+            }
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            erros = PrioridadePaa.excluir_em_lote(lista_uuids)
+            if len(erros):
+                mensagem = 'Alguma das prioridades selecionadas já foi removida.'
+            else:
+                mensagem = 'Prioridades removidas com sucesso.'
+            return Response({
+                'erros': erros,
+                'mensagem': mensagem
+            }, status=status.HTTP_200_OK)
+
+        except Exception as err:
+            error = {
+                'erro': "Falha ao excluir Prioridades em lote",
+                'mensagem': str(err)
+            }
+            return Response(error, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        """
+        Cenário de exceção: quando tentar atualizar uma prioridade que já foi removida
+        """
+        try:
+            self.get_object()
+            return super().update(request, *args, **kwargs)
+        except (Http404, NotFound):
+            return Response(
+                {"mensagem": "Prioridade já foi removida da base de dados."},
+                status=status.HTTP_404_NOT_FOUND
+            )
