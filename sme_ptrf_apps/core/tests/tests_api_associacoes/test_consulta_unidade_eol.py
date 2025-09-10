10898149fbcf68090eaa262cab2b20d6a387eab0
Luís Dourado <luis_dourado33@hotmail.com>
2025-09-10 14:37:18 -0400

    test(133162): Adiciona teste para validação do campo 'nome_dre' na consulta de unidade

--- sme_ptrf_apps/core/tests/tests_api_associacoes/test_consulta_unidade_eol.py
+++ sme_ptrf_apps/core/tests/tests_api_associacoes/test_consulta_unidade_eol.py
@@ -29,3 +29,30 @@ def test_consulta_unidade(jwt_authenticated_client_a):
 
         assert response.status_code == status.HTTP_200_OK
         assert result == resultado_esperado
+
+
+def test_consulta_unidade_retorna_nome_dre(jwt_authenticated_client_a):
+    from unittest.mock import patch
+    with patch('sme_ptrf_apps.core.api.views.associacoes_viewset.consulta_unidade') as mock_patch:
+        resultado_esperado = {
+            'codigo_eol': "786543",
+            'nome': 'Unidade Nova',
+            'nome_dre': 'DRE Butantã',
+            'email': 'unidadenova@gmail.com',
+            'telefone': '11992735056',
+            'numero': '89',
+            'tipo_logradouro': 'Rua',
+            'logradouro': 'Flamengo',
+            'bairro': 'Ferreira',
+            'cep': '05524160'
+        }
+
+        mock_patch.return_value = resultado_esperado
+
+        response = jwt_authenticated_client_a.get(
+            f'/api/associacoes/eol/?codigo_eol=786543', content_type='application/json')
+        result = json.loads(response.content)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert 'nome_dre' in result
+        assert result['nome_dre'] == resultado_esperado['nome_dre']
