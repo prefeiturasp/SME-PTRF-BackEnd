a962c55e44af1312c5df89b912a05b615645186b
Wilson Santos <wilson.santos@spassu.com.br>
2025-11-28 11:00:47 -0300

    Ajuste automação de testes

--- tests/api/cypress/e2e/PTRF/api/CT95-Consulta_Creditos_Escola_Arredondamento_Sem_Retorno.cy.js
+++ tests/api/cypress/e2e/PTRF/api/CT95-Consulta_Creditos_Escola_Arredondamento_Sem_Retorno.cy.js
@@ -8,23 +8,18 @@ const usuario = usuarios.Kellen;
 import ComumPaginaPTRF from "../../../support/Paginas/ComumPaginaPTRF";
 const Comum = new ComumPaginaPTRF();
 
-import GastosEscolaPagina from "../../../support/Paginas/GastosEscolaPagina";
+import GastosEscolaPagina from "../../../support/Paginas/CreditosEscolaPagina";
 const Gastos = new GastosEscolaPagina();
 
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // quando retorna falso previne o  Cypress de falhar o teste
+  return false;
+});
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // quando retorna falso previne o  Cypress de falhar o teste
-    return false
-  })
-
-  describe('Gastos da Escola - Editar', () => {
-
-    it('CT94-Editar_Gastos_Escola_Cadastro_Estorno_Salvar_Data',()=>{
-
+describe("Credito Escola - Consulta", () => {
+  it("CT95-Consulta_Escola_Arredondamento_Sem_Retorno", () => {
     Comum.visitarPaginaPTRF();
 
     Comum.login(usuario.Usuario, usuario.Senha);
-    
-  })  
-
-})
+  });
+});
--- tests/api/cypress/e2e/PTRF/api/CT99-Consulta_Creditos_Escola_Rendimento.cy.js
+++ tests/api/cypress/e2e/PTRF/api/CT99-Consulta_Creditos_Escola_Rendimento.cy.js
@@ -8,23 +8,18 @@ const usuario = usuarios.Kellen;
 import ComumPaginaPTRF from "../../../support/Paginas/ComumPaginaPTRF";
 const Comum = new ComumPaginaPTRF();
 
-import GastosEscolaPagina from "../../../support/Paginas/GastosEscolaPagina";
+import GastosEscolaPagina from "../../../support/Paginas/CreditosEscolaPagina";
 const Gastos = new GastosEscolaPagina();
 
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // quando retorna falso previne o  Cypress de falhar o teste
+  return false;
+});
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // quando retorna falso previne o  Cypress de falhar o teste
-    return false
-  })
-
-  describe('Gastos da Escola - Consulta', () => {
-
-    it('CT63-Consulta_Gastos_Escola_Acao_Material_Complementar',()=>{
-
+describe("Credito Escola - Consulta", () => {
+  it("CT99-Consulta_Creditos_Escola_Rendimento", () => {
     Comum.visitarPaginaPTRF();
 
     Comum.login(usuario.Usuario, usuario.Senha);
-    
-  })  
-
-})
+  });
+});
