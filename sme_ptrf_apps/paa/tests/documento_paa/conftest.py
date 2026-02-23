45e8251d5de0f72af60164d3f8e48cb2fda48b07
Agostinho <jr.jardimmatos@gmail.com>
2026-02-23 14:58:24 -0300

    test: Testes adicionais no backend

--- sme_ptrf_apps/paa/tests/documento_paa/conftest.py
+++ sme_ptrf_apps/paa/tests/documento_paa/conftest.py
@@ -1,8 +1,9 @@
 import pytest
+from django.contrib.admin.sites import site
+from django.contrib.auth import get_user_model
 
 from sme_ptrf_apps.paa.models import DocumentoPaa
 from sme_ptrf_apps.paa.admin import DocumentoPaaAdmin
-from django.contrib.admin.sites import site
 
 
 @pytest.fixture
@@ -13,3 +14,13 @@ def documento_paa_admin():
 @pytest.fixture
 def documento_paa(documento_paa_factory):
     return documento_paa_factory.create()
+
+
+@pytest.fixture
+def usuario_task():
+    User = get_user_model()
+    return User.objects.create_user(
+        username='usuario_task_test',
+        password='senha123',
+        email='task@teste.com'
+    )
