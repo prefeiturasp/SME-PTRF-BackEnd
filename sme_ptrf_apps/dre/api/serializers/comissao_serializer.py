eb02169f365374f38869763573ebad5d1d8a1d69
Maycon Silva <maycon.silva@spassu.com.br>
2026-06-08 14:28:11 -0300

    feat(148897): add endpoints comissões para parametrizações
    - Adiciona nova validação para edição para dre e recurso diferente;
    - Adiciona endpoints para comissão para consumo em front de parametrizações;

--- sme_ptrf_apps/dre/api/serializers/comissao_serializer.py
+++ sme_ptrf_apps/dre/api/serializers/comissao_serializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from sme_ptrf_apps.core.models.recurso import Recurso
+from sme_ptrf_apps.dre.models import comissao
 from ...models import Comissao
 
 
@@ -7,3 +9,95 @@ class ComissaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comissao
         fields = ('uuid', 'id', 'nome')
+
+
+class SimpleRecursoSerializer(serializers.Serializer):
+    uuid = serializers.UUIDField()
+    nome = serializers.CharField(max_length=255)
+
+class ComissaoParametrizacaoSerializer(serializers.ModelSerializer):
+    recursos = SimpleRecursoSerializer(many=True, read_only=True)
+    class Meta:
+        model = Comissao
+        fields = ('uuid', 'id', 'nome', 'responsavel_analise_pc', 'recursos')
+
+
+    def create(self, validated_data):
+        recursos_data = self.initial_data.get('recursos', [])
+        recursos = []
+
+        for recurso_uuid in recursos_data:
+            if recurso_uuid:
+                try:
+                    recurso = Recurso.objects.get(uuid=recurso_uuid)
+                    recursos.append(recurso)
+                except ValueError:
+                    continue
+                except Recurso.DoesNotExist:
+                    continue
+
+        if len(recursos) <= 0:
+            raise serializers.ValidationError({
+                "non_field_errors": "Pelo menos um recurso válido deve ser associado à comissão."
+            })
+
+        nome = validated_data.get('nome')
+        nome = ' '.join(nome.split()) if nome else ''
+        validated_data['nome'] = nome
+
+        is_valid, error_message = Comissao.is_valid_data(
+            nome=nome,
+            recursos=recursos,
+            responsavel_analise_pc=validated_data.get('responsavel_analise_pc', False)
+        )
+
+        if not is_valid:
+            raise serializers.ValidationError({
+                "non_field_errors": error_message
+            })
+
+        instance = super().create(validated_data)
+        instance.recursos.set(recursos)
+
+        return instance
+
+
+    def update(self, instance, validated_data):
+        recursos_data = self.initial_data.get('recursos', [])
+        recursos = []
+
+        for recurso_uuid in recursos_data:
+            if recurso_uuid:
+                try:
+                    recurso = Recurso.objects.get(uuid=recurso_uuid)
+                    recursos.append(recurso)
+                except ValueError:
+                    continue
+                except Recurso.DoesNotExist:
+                    continue
+
+        if len(recursos) <= 0:
+            raise serializers.ValidationError({
+                "non_field_errors": "Pelo menos um recurso válido deve ser associado à comissão."
+            })
+
+        nome = validated_data.get('nome')
+        nome = ' '.join(nome.split()) if nome else ''
+        validated_data['nome'] = nome
+
+        is_valid, error_message = Comissao.is_valid_data(
+            nome=nome,
+            recursos=recursos,
+            responsavel_analise_pc=validated_data.get('responsavel_analise_pc', False),
+            instance_id=instance.id
+        )
+
+        if not is_valid:
+            raise serializers.ValidationError({
+                "non_field_errors": error_message
+            })
+
+        instance = super().update(instance, validated_data)
+        instance.recursos.set(recursos)
+
+        return instance
--- sme_ptrf_apps/dre/models/comissao.py
+++ sme_ptrf_apps/dre/models/comissao.py
@@ -21,12 +21,43 @@ class Comissao(ModeloIdNome):
         verbose_name = "Comissão"
         verbose_name_plural = "Comissões"
 
+    @staticmethod
+    def get_membros_comissao(comissao_instance):
+        return comissao_instance.membros.all()
+
+
+    @classmethod
+    def verifica_dres_membros_comissao_tem_recursos(cls, comissao_instance, recursos):
+        from sme_ptrf_apps.core.services.recursos_service import RecursoService
+
+        membros_comissao = cls.get_membros_comissao(comissao_instance)
+        list_recursos_id = [recurso.id for recurso in recursos]
+
+        for membro in membros_comissao:
+            recursos_dre_membro = RecursoService.por_dre(membro.dre).values_list('id', flat=True)
+
+            existe_recurso_comum = any(recurso_id in list_recursos_id for recurso_id in recursos_dre_membro)
+            if not existe_recurso_comum:
+                return False
+
+        return True
+
+
     @classmethod
     def is_valid_data(cls, nome, recursos=None, responsavel_analise_pc=False, instance_id=None):
+        comissao = None
         nome = ' '.join(nome.split()) if nome else ''
 
         if not nome:
             return False, "O campo 'nome' é obrigatório."
+
+        if instance_id:
+            try:
+                comissao = cls.objects.get(pk=instance_id)
+            except cls.DoesNotExist:
+                return False, "Comissão não encontrada para o ID fornecido."
+            except Exception:
+                return False, "Ocorreu um erro ao validar a comissão."
 
         if recursos is None or not isinstance(recursos, list) or len(recursos) <= 0:
             return False, "O campo 'recursos' é obrigatório"
@@ -34,7 +65,7 @@ class Comissao(ModeloIdNome):
         already_exists_with_same_name_and_recursos = cls.objects.filter(
             nome=nome,
             recursos__in=recursos
-        ).exclude(id=instance_id).exists()
+        ).exclude(id=comissao.id).exists()
 
         if already_exists_with_same_name_and_recursos:
             return False, "Já existe uma comissão com o mesmo nome no recurso selecionado."
@@ -43,10 +74,14 @@ class Comissao(ModeloIdNome):
             already_exists_responsavel_analise_pc = cls.objects.filter(
                 responsavel_analise_pc=responsavel_analise_pc,
                 recursos__in=recursos
-            ).exclude(id=instance_id).exists()
+            ).exclude(id=comissao.id).exists()
 
             if already_exists_responsavel_analise_pc:
                 return False, "Um ou mais recursos selecionados já estão associados a uma comissão de análise de prestação de contas."
+
+        if instance_id and len(cls.get_membros_comissao(comissao)) > 0:
+            if not cls.verifica_dres_membros_comissao_tem_recursos(comissao, recursos):
+                return False, "Não é possível editar esta comissão, pois ela já possui membros associados que não têm acesso aos recursos selecionados."
 
         return True, ""
 
