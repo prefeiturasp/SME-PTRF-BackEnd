6e69102ee70d4893b67bbf79e75ce93da9222537
Maycon Silva <maycon.silva@spassu.com.br>
2026-06-10 12:08:58 -0300

    refac(149762): remove campos de parâmetros de contas e ações
    - campos: permite_saldo_conta_negativo e permite_saldo_acoes_negativo;

--- sme_ptrf_apps/core/models/parametros.py
+++ sme_ptrf_apps/core/models/parametros.py
@@ -11,9 +11,6 @@ from auditlog.registry import auditlog
 
 class Parametros(SingletonModel, ModeloBase):
     history = AuditlogHistoryField()
-    permite_saldo_conta_negativo = models.BooleanField('Permite saldo negativo em contas?', default=True) # TODO: Remover! Este campo foi movido para recurso
-
-    permite_saldo_acoes_negativo = models.BooleanField('Permite saldo negativo em ações?', default=True) # TODO: Remover! Este campo foi movido para recurso
 
     fique_de_olho = RichTextField(null=True)
     fique_de_olho_relatorio_dre = RichTextField(null=True, verbose_name='Fique de olho (Relatório DRE)')
