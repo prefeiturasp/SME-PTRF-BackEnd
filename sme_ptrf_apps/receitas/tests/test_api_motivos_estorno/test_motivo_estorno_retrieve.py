870efb96fd6f3fe1371f8107804ad9867d0e28d8
ollyvergithub <ollyverottoboni@gmail.com>
2026-07-24 08:01:45 -0300

    test: Adiciona recurso nos testes de motivos de estorno e corrige tipo de conteúdo

--- sme_ptrf_apps/receitas/tests/test_api_motivos_estorno/test_motivo_estorno_retrieve.py
+++ sme_ptrf_apps/receitas/tests/test_api_motivos_estorno/test_motivo_estorno_retrieve.py
@@ -8,7 +8,7 @@ pytestmark = pytest.mark.django_db
 def test_retrieve_motivo_estorno(jwt_authenticated_client_p, motivo_estorno_01):
     response = jwt_authenticated_client_p.get(
         f'/api/motivos-estorno/{motivo_estorno_01.uuid}/',
-        content_type='applicaton/json'
+        content_type='application/json'
     )
 
     result = json.loads(response.content)
@@ -16,7 +16,8 @@ def test_retrieve_motivo_estorno(jwt_authenticated_client_p, motivo_estorno_01):
     resultado_esperado = {
         'id': motivo_estorno_01.id,
         'uuid': f'{motivo_estorno_01.uuid}',
-        'motivo': 'Motivo de estorno 01'
+        'motivo': 'Motivo de estorno 01',
+        'recurso': f'{motivo_estorno_01.recurso.uuid}'
     }
 
     assert response.status_code == status.HTTP_200_OK
