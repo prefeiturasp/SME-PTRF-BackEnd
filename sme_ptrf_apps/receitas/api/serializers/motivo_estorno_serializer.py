1aa70462db9d2c52c2b9554cf0f30f77c1624b41
ollyvergithub <ollyverottoboni@gmail.com>
2026-07-24 08:04:51 -0300

    feat: Implementa Motivos de Estorno separar por recurso em Parametrizações

--- sme_ptrf_apps/receitas/api/serializers/motivo_estorno_serializer.py
+++ sme_ptrf_apps/receitas/api/serializers/motivo_estorno_serializer.py
@@ -1,8 +1,42 @@
 from rest_framework import serializers
+from sme_ptrf_apps.core.models import Recurso
+from sme_ptrf_apps.utils.update_instance_from_dict import update_instance_from_dict
 from ...models import MotivoEstorno
 
 
 class MotivoEstornoSerializer(serializers.ModelSerializer):
+
+    recurso = serializers.SlugRelatedField(
+        slug_field='uuid',
+        required=True,
+        queryset=Recurso.objects.all()
+    )
+
+    def create(self, validated_data):
+        motivo=validated_data['motivo']
+        recurso = validated_data.get('recurso', None)
+
+        motivo_ja_cadastrado = MotivoEstorno.objects.filter(motivo=motivo, recurso=recurso).all()
+        if motivo_ja_cadastrado:
+            raise serializers.ValidationError('Motivo já cadastrado para este recurso.')
+
+        motivo_estorno_criado = MotivoEstorno.objects.create(**validated_data)
+
+        return motivo_estorno_criado
+
+    def update(self, instance, validated_data):
+        motivo = validated_data.get("motivo", None)
+        recurso = validated_data.get("recurso", instance.recurso)
+
+        if motivo and instance.motivo != motivo:
+            motivo_ja_cadastrado = MotivoEstorno.objects.filter(motivo=motivo, recurso=recurso).all()
+            if motivo_ja_cadastrado:
+                raise serializers.ValidationError('Motivo já cadastrado para este recurso.')
+
+        update_instance_from_dict(instance, validated_data, save=True)
+
+        return instance
+
     class Meta:
         model = MotivoEstorno
-        fields = ('id', 'uuid', 'motivo', )
+        fields = ('id', 'uuid', 'motivo', 'recurso')
